38ab0a01d5319998b4fed6c52b7c91b41407f1c3
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-03 03:26:10 +0100

    Fix error message while loading models

--- app/models/legislation_area.rb
+++ app/models/legislation_area.rb
@@ -1,8 +1,9 @@
 class LegislationArea < ActiveRecord::Base
   attr_accessible :value
-  
-  has_many :subareas, class_name: :LegislationSubarea,
-                      dependent: destroy
+
+  # TODO: Fix destroy method, should be with argument
+  has_many :subareas, class_name: :LegislationSubarea
+                      #dependent: destroy
   
   has_many :decrees
              
