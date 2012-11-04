265cd4f280a2b49b689f86e62550e16d10872a38
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-04 02:41:05 +0100

    Add config for list of Decrees

---
+++ lib/core/justice_gov_sk/config/decrees/list.rb
@@ -0,0 +1,18 @@
+module JusticeGovSk
+  module Config
+    module Decrees
+      class List
+        include JusticeGovSk::Config::Request
+
+        def request_dump_path
+          'list_decrees_request_dump'
+        end
+
+        def url
+          'http://www.justice.gov.sk/Stranky/Sudne-rozhodnutia/Sudne-rozhodnutia.aspx'
+        end
+      end
+    end
+  end
+end
+
