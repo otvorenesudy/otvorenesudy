3484a341ff315262746ec30f05d48042a717ea84
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-04 01:55:36 +0100

    Refactor quotes

--- lib/core/justice_gov_sk/config/hearings/criminal_list.rb
+++ lib/core/justice_gov_sk/config/hearings/criminal_list.rb
@@ -5,11 +5,11 @@ module JusticeGovSk
         include JusticeGovSk::Config::Request
 
         def request_dump_path
-          "criminal_hearing_request_dump"
+          'criminal_list_hearings_request_dump'
         end
 
         def url
-          "http://www.justice.gov.sk/Stranky/Pojednavania/PojednavanieTrestZoznam.aspx"
+          'http://www.justice.gov.sk/Stranky/Pojednavania/PojednavanieTrestZoznam.aspx'
         end
       end
     end
