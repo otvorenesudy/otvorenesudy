e9698315f0924de6e5ffe7802e0794b38883243f
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-04 01:55:56 +0100

    Add new configs for Special court and Civil hearings

---
+++ lib/core/justice_gov_sk/config/hearings/civil_list.rb
@@ -0,0 +1,18 @@
+module JusticeGovSk
+  module Config
+    module Hearings
+      class CivilList
+        include JusticeGovSk::Config::Request
+
+        def request_dump_path
+          'civil_list_hearings_request_dump'
+        end
+
+        def url
+          'http://www.justice.gov.sk/Stranky/Pojednavania/PojednavanieZoznam.aspx'
+        end
+      end
+    end
+  end
+end
+
---
+++ lib/core/justice_gov_sk/config/hearings/special_criminal_list.rb
@@ -0,0 +1,18 @@
+module JusticeGovSk
+  module Config
+    module Hearings
+      class SpecialCriminalList
+        include JusticeGovSk::Config::Request
+
+        def request_dump_path
+          'special_criminal_list_hearings_request_dump'
+        end
+
+        def url
+          'http://www.justice.gov.sk/Stranky/Pojednavania/PojednavanieSpecZoznam.aspx'
+        end
+      end
+    end
+  end
+end
+
