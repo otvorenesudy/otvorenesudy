9060d55a34802dad01a5cad500440971773a43cc
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-04 01:51:42 +0100

    Add new tests for Downloader

--- spec/core/downloader_spec.rb
+++ spec/core/downloader_spec.rb
@@ -1,22 +1,45 @@
 # encoding: utf-8
-require 'spec_helper'
+require 'downloader_spec_helper'
 
-describe "Downloader" do 
-  it "should download first page of hearings with 100 results" do
-    request = JusticeGovSk::Config::Hearings::CriminalList.new 
-    downloader = Downloader.new
+describe Downloader do 
+  before :each do 
+    @downloader = Downloader.new
+    @downloader.wait_time = 0
+    @downloader.cache_store = true
+    @downloader.cache_load = false
+    # cache root
+    #@downloader.cache_root = DOWNLOADER_FIXTURE_PATH
+  end
 
-    downloader.wait_time = 0.seconds
+  after :each do
+    @downloader.headers = @request.headers
+    @downloader.data = @request.data
 
-    # it is possible to chain methods like .page(1).count(1)
-    request.page(1)
-    request.count(100)
+    html = @downloader.download(@request.url)
 
-    downloader.headers = request.headers
-    downloader.data = request.data
+    @downloader.response_code.should == 200
+    html.should_not be_nil
+  end
+
+  it "should download first page of criminal hearings with 100 results" do
+    @request = JusticeGovSk::Config::Hearings::CriminalList.new 
     
-    data = downloader.download(request.url)
+    # it is possible to chain methods like .page(1).count(1)
+    @request.page(1)
+    @request.count(100)
+  end
 
-    data.should_not be_nil
+  it 'should download first page of civil hearing with 20 results' do
+    @request = JusticeGovSk::Config::Hearings::CivilList.new 
+    
+    @request.page(1)
+    @request.count(20)
+  end
+
+  it 'should download first page of special criminal hearings page with 50 results' do
+    @request = JusticeGovSk::Config::Hearings::SpecialCriminalList.new
+
+    @request.page(1)
+    @request.count(50)
   end
 end
