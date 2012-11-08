7f2ba6f82844129cc815f6b3abbbc80ce78a48d6
Pavol Zbell <pavol.zbell@gmail.com>
2012-11-08 03:09:28 +0100

    Add part of hearing crawlers and parsers

--- lib/core/justice_gov_sk/crawlers/civil_hearing_crawler.rb
+++ lib/core/justice_gov_sk/crawlers/civil_hearing_crawler.rb
@@ -22,14 +22,35 @@ module JusticeGovSk
       
       def judges(document)
         names = @parser.judges(document)
-        
-        puts names
+    
+        unless names.empty?
+          puts "Processing #{pluralize names.count, 'judges'}."
+          
+          names.each do |name|
+            judge = judge_factory.find(name)
+            
+            unless judge.nil?
+              judging(judge)
+            end
+          end
+        end
       end
         
       def proposers(document)
       end
 
       def opponents(document)
+      end
+      
+      private
+      
+      def judging(judge)
+        judging = judging_factory.find_or_create(judging.id, @hearing.id)
+        
+        judging.judge   = judge
+        judging.hearing = @hearing
+        
+        @persistor.persist(judging) if judging.id.nil?
       end
     end
   end
