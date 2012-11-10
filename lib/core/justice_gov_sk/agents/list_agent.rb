217c4474b3f962a22c7e1b41a3734fb59d469296
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-10 16:56:37 +0100

    Add selecting a form of a Decree to ListAgent

--- lib/core/justice_gov_sk/agents/list_agent.rb
+++ lib/core/justice_gov_sk/agents/list_agent.rb
@@ -3,9 +3,10 @@ module JusticeGovSk
     class ListAgent < Agent
       attr_accessor :form_name,
                     :per_page_field_name,
-                    :page_field_name,
-                    :old_records_checkbox_name,
-                    :form
+                    :page_field_name
+
+      attr_accessor :hearing_or_decree_include_old_checkbox_name,
+                    :decree_form_select_box_name
 
       def initialize
         super
@@ -21,17 +22,26 @@ module JusticeGovSk
         super(request) do |page|
           @form = page.form_with(name: @form_name)
           
-          # Set per_page count
           fields = @form.fields.map(&:name)
-          @per_page_field_name = fields.find { |field| field.match(/\A.+cmbAGVCountOnPage\Z/) } unless @per_page_field_name
 
-          # Check old records
-          if request.include_old_records
-            checkboxes = @form.checkboxes.map(&:name)
-            @old_records_checkbox_name = checkboxes.find { |field| field.match(/\A.+StarsiePojednavania\Z/) } unless @old_records_field_name
-            @form.checkbox_with(name: @old_records_checkbox_name).check if @old_records_checkbox_name
+          # Forms for DecreeList 
+          if request.decree_form
+            @decree_form_select_box_name = fields.find { |field| field.match(/\A.+cmbForma\Z/) } unless @decree_form_select_box_name
+            field = @form.field_with(:name => @decree_form_select_box_name) if @decree_form_select_box_name 
+            field.value = request.decree_form
           end
 
+          # Check old records
+          if request.hearing_or_decree_include_old
+            checkboxes = @form.checkboxes.map(&:name)
+            @hearing_or_decree_include_old_checkbox_name = checkboxes.find { |field| field.match(/\A.+StarsiePojednavania\Z/) } unless @hearing_or_decree_include_old_checkbox_name
+            @form.checkbox_with(name: @hearing_or_decree_include_old_checkbox_name).check if @hearing_or_decree_include_old_checkbox_name
+          end
+
+          @form = page.form_with(name: @form_name)
+
+          # Set per_page count
+          @per_page_field_name = fields.find { |field| field.match(/\A.+cmbAGVCountOnPage\Z/) } unless @per_page_field_name
           if request.per_page
             @form.field_with(name: @per_page_field_name).value = request.per_page if @per_page_field_name
             postback_fields(@per_page_field_name, '') 
@@ -47,7 +57,7 @@ module JusticeGovSk
           if request.page 
             @form.field_with(name: @page_field_name).value = request.page if @page_field_name
             postback_fields(@page_field_name, '')
-            page = form.submit
+            page = @form.submit
           end
 
          page 
