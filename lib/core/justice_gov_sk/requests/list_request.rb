44252a387a6c7fcca13a4c314837e595f7bd4947
Pavol Zbell <pavol.zbell@gmail.com>
2012-11-10 17:18:15 +0100

    Clean up in list request

--- lib/core/justice_gov_sk/requests/list_request.rb
+++ lib/core/justice_gov_sk/requests/list_request.rb
@@ -10,17 +10,17 @@ module JusticeGovSk
       
       def headers
         {
-          'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-          'Accept-Charset' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 
+          'Accept'          => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+          'Accept-Charset'  => 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 
           'Accept-Encoding' => 'gzip,deflate,sdch',
           'Accept-Language' => 'en-US,en;q=0.8',
-          'Cache-Control' => 'max-age=0',
-          'Connection' => 'keep-alive',
-          'Content-Type' => 'application/x-www-form-urlencoded',
-          'Host' => 'www.justice.gov.sk',
-          'Origin' => 'http://www.justice.gov.sk',
-          'Cookie' => '__utma=161066278.1275351275.1351816765.1352194655.1352230438.9; __utmz=161066278.1351816765.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmb=161066278.26.10.1352230438; __utmc=161066278; ASP.NET_SessionId=rqpypa55ahrhpge5valxzv45',
-          'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
+          'Cache-Control'   => 'max-age=0',
+          'Connection'      => 'keep-alive',
+          'Content-Type'    => 'application/x-www-form-urlencoded',
+          'Host'            => 'www.justice.gov.sk',
+          'Origin'          => 'http://www.justice.gov.sk',
+          'Cookie'          => '__utma=161066278.1275351275.1351816765.1352194655.1352230438.9; __utmz=161066278.1351816765.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmb=161066278.26.10.1352230438; __utmc=161066278; ASP.NET_SessionId=rqpypa55ahrhpge5valxzv45',
+          'User-Agent'      => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
         }
       end
       
@@ -36,10 +36,6 @@ module JusticeGovSk
 
       def page=(value)
         data.gsub!(/cmbAGVPager=\d+&/, "cmbAGVPager=#{value.to_i}&")
-        
-        # TODO rm
-          puts "XXXX #{page}"
-          puts "XXXX #{per_page}"
       end
       
       def page
@@ -52,13 +48,6 @@ module JusticeGovSk
 
       def per_page
         data.match(/cmbAGVCountOnPage=(?<value>\d+)&/)[:value].to_i
-      end
-
-      # TODO rm, still used?
-      def save_data
-        File.open(data_path, "w") do |f|
-          f.write data
-        end
       end
     end
   end
