862c0f092c9f3d84622de88242f8809b3e74d2f4
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-10 22:00:51 +0100

    Refactor ListRequest test

--- spec/core/justice_gov_sk/agents/list_agent_spec.rb
+++ spec/core/justice_gov_sk/agents/list_agent_spec.rb
@@ -20,33 +20,33 @@ describe JusticeGovSk::Agents::ListAgent do
 
   describe "for Hearing List request" do
     it "should download 200th page of Criminal Hearings with 100 results including old records" do
-      @request = JusticeGovSk::AgentRequests::CriminalHearingListRequest.new
+      @request = JusticeGovSk::Requests::CriminalHearingListRequest.new
 
       @request.page = 200 
       @request.per_page = 100 
-      @request.hearing_or_decree_include_old = true
-   end
+      @request.include_old_hearing_or_decree = true
+    end
 
     it "should download 400th page of Civil Hearings with 100 results inluding old records" do
-      @request = JusticeGovSk::AgentRequests::CivilHearingListRequest.new
+      @request = JusticeGovSk::Requests::CivilHearingListRequest.new
 
       @request.page = 400 
       @request.per_page = 100 
-      @request.hearing_or_decree_include_old = true
+      @request.include_old_hearing_or_decree = true
     end
 
     it "should download 4th page of Special Hearings with 20 results including old records" do
-      @request = JusticeGovSk::AgentRequests::SpecialHearingListRequest.new
+      @request = JusticeGovSk::Requests::SpecialHearingListRequest.new
 
       @request.page = 4 
       @request.per_page = 20 
-      @request.hearing_or_decree_include_old = true
+      @request.include_old_hearing_or_decree = true
     end
   end
 
   describe "for Judge List request" do
     it "should download 25th page with 50 results" do
-      @request = JusticeGovSk::AgentRequests::JudgeListRequest.new
+      @request = JusticeGovSk::Requests::JudgeListRequest.new
 
       @request.page = 4 
       @request.per_page = 20 
@@ -55,7 +55,7 @@ describe JusticeGovSk::Agents::ListAgent do
 
   describe "for Court List request" do
     it "should download first page with 100 results" do
-      @request = JusticeGovSk::AgentRequests::CourtListRequest.new
+      @request = JusticeGovSk::Requests::CourtListRequest.new
 
       @request.page = 1 
       @request.per_page = 100
@@ -64,14 +64,14 @@ describe JusticeGovSk::Agents::ListAgent do
 
   describe "for Decree List request" do
     it "should download 3500th page with 100 results including old records" do
-      @request = JusticeGovSk::AgentRequests::DecreeListRequest.new
+      @request = JusticeGovSk::Requests::DecreeListRequest.new
 
       @request.page = 3500 
       @request.per_page = 100 
     end
 
     it "should download 20th page with 100 results with form value 'R'" do 
-      @request = JusticeGovSk::AgentRequests::DecreeListRequest.new
+      @request = JusticeGovSk::Requests::DecreeListRequest.new
 
       @request.page = 15 
       @request.per_page = 100 
