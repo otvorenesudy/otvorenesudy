7481992bc77d02dbf37432d0fc1255d3337a0c76
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-10 21:44:35 +0100

    Add HearingListRequest

---
+++ lib/core/justice_gov_sk/requests/hearing_list_request.rb
@@ -0,0 +1,15 @@
+module JusticeGovSk
+  module Requests
+    class CivilHearingListRequest < JusticeGovSk::Requests::HearingListRequest
+
+      def initialize
+        super
+        @include_old_hearing_or_decree = true
+      end
+
+      def url
+        "#{JusticeGovSk::Requests::URL.base}/Stranky/Pojednavania/PojednavanieZoznam.aspx"
+      end
+    end
+  end
+end
