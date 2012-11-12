fb0566fca47f665ffa60f18c78cd451943f8124d
Pavol Zbell <pavol.zbell@gmail.com>
2012-11-12 20:08:44 +0100

    Add conversion from UTF-8 to ASCII, no diacritics

--- lib/core/classes/string.rb
+++ lib/core/classes/string.rb
@@ -3,6 +3,12 @@ require 'active_support/multibyte'
 class String
   alias :utf8 :mb_chars
   
+  def ascii
+    return self if self.ascii_only?
+    
+    value.utf8.normalize(:kd).bytes.map { |b| (0x00..0x7F).include?(b) ? b.chr : '' }.join
+  end
+  
   def upcase_first
     self.sub(/^\D{0,1}/) { |c| c.upcase }
   end
