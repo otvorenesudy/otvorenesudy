ea3eea9c41e7030723a1a3960b15a043a413b3e6
Pavol Zbell <pavol.zbell@gmail.com>
2012-11-15 00:30:06 +0100

    Adjust member visibility in text extractor class

--- lib/core/justice_gov_sk/documents/text_extractor.rb
+++ lib/core/justice_gov_sk/documents/text_extractor.rb
@@ -1,29 +1,12 @@
 require 'docsplit'
 
 class TextExtractor
-  include Cache
-  
-  alias :cache_root= :root=
-  alias :cache_root  :root
-
   attr_accessor :use_ocr
 
   def initialize
     @document_formats = [:pdf]
     @cache_path       = File.join(Rails.root, 'tmp', 'documents', 'extracted')
     @use_ocr          = false
-  end
-
-  def preextract(path)
-    raise "File not exists (#{path})" if not File.exists?(path)
-
-    extension = path.split('.').last.to_sym
-
-    raise 'Unsupported document type' if not @document_formats.include?(extension)
-
-    FileUtils.mkpath(cache_root) if not File.exists?(cache_root)
-    
-    return path, extension
   end
 
   def extract(path)
@@ -42,5 +25,26 @@ class TextExtractor
       content
     end
   end
+
+  protected
+  
+  def preextract(path)
+    raise "File not exists (#{path})" if not File.exists?(path)
+
+    extension = path.split('.').last.to_sym
+
+    raise 'Unsupported document type' if not @document_formats.include?(extension)
+
+    FileUtils.mkpath(cache_root) if not File.exists?(cache_root)
+    
+    return path, extension
+  end
+
+  private
+  
+  include Cache
+  
+  alias :cache_root= :root=
+  alias :cache_root  :root
 end
 
