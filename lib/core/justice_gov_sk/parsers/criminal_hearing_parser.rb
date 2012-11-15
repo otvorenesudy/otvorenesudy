adff7da6095d5a5dc8e8fce47639a59d962a5350
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-15 03:50:27 +0100

    Fix spaces and other bugs in DecreeParser

--- lib/core/justice_gov_sk/parsers/criminal_hearing_parser.rb
+++ lib/core/justice_gov_sk/parsers/criminal_hearing_parser.rb
@@ -17,7 +17,7 @@ module JusticeGovSk
               name = div.text.strip
               map[name] = []
             elsif div[:class] == 'hodnota'
-              map[name] << div.text.strip
+              map[name] << div.text.gsub(/\A-\s+§/, '')
             end
           end
 
