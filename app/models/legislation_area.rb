73dc5a9d34be5fb828fc08a283b48b647558c086
Pavol Zbell <pavol.zbell@gmail.com>
2012-11-16 23:12:00 +0100

    Fix relation in LegislationArea to subareas

--- app/models/legislation_area.rb
+++ app/models/legislation_area.rb
@@ -1,9 +1,8 @@
 class LegislationArea < ActiveRecord::Base
   attr_accessible :value
 
-  # TODO: Fix destroy method, should be with argument
-  has_many :subareas, class_name: :LegislationSubarea
-                      #dependent: destroy
+  has_many :subareas, class_name: :LegislationSubarea,
+                      dependent: :destroy
   
   has_many :decrees
              
