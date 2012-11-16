717d9ea018c427e33bbc1ea0eb26cd1fe2ebe93e
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-16 20:38:20 +0100

    Comment out Postgre specific migrations

--- db/migrate/20121110160219_add_gin_index_to_court_name.rb
+++ db/migrate/20121110160219_add_gin_index_to_court_name.rb
@@ -1,9 +1,9 @@
 class AddGinIndexToCourtName < ActiveRecord::Migration
   def up
-    add_index :courts, :name, gin: true, name: 'gin_index_courts_on_name' 
+    #add_index :courts, :name, gin: true, name: 'gin_index_courts_on_name' 
   end
 
   def down
-    remove_index :courts, name: 'gin_index_courts_on_name'
+    #remove_index :courts, name: 'gin_index_courts_on_name'
   end
 end
