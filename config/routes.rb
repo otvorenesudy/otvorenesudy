7a6fcd625c1bfeaf9da77afa77a983dc1655d51d
Samuel Molnar <molnar.samuel@gmail.com>
2012-11-21 17:04:15 +0100

    Add resque server to routes
    Signed-off-by: Samuel Molnar <molnar.samuel@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,10 @@ Otvorenesudy::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  #
+  #
+
+
+  # Resque
+  mount Resque::Server.new, :at => "/resque"
 end
