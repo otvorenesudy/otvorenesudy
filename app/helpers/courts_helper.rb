b8a421d0d19839703cce61598450369c48ee5af8
Pavol Zbell <pavol.zbell@gmail.com>
2012-11-22 16:28:21 +0100

    Add some helper methods

--- app/helpers/courts_helper.rb
+++ app/helpers/courts_helper.rb
@@ -1,2 +1,5 @@
 module CourtsHelper
+  def link_to_court(court)
+    link_to court.name, courts_path(court.id)
+  end
 end
--- app/helpers/judges_helper.rb
+++ app/helpers/judges_helper.rb
@@ -1,2 +1,5 @@
 module JudgesHelper
+  def link_to_judge(judge)
+    link_to judge.name, judges_path(judge.id)
+  end
 end
