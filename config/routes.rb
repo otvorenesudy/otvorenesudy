e748af7968e3f6f364356e836c5a21f21048e1ba
Pavol Zbell <pavol.zbell@gmail.com>
2012-11-22 16:47:54 +0100

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Otvorenesudy::Application.routes.draw do
+  root :to => 'static_pages#home'
+  
+  match '/about',   :to => 'static_pages#about'
+  match '/contact', :to => 'static_pages#contact'
+  match '/home',    :to => 'static_pages#home'
+  
   resources :courts
   resources :judges
   resources :hearings
