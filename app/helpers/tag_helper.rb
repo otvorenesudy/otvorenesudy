22c230a8e1ae5bd681ad5e70bc46affb6caff50d
Pavol Zbell <pavol.zbell@gmail.com>
2012-11-28 21:00:46 +0100

    Refactor navigation bar and icons

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -1,10 +1,17 @@
 module TagHelper
-  def badge_tag(text, *tags)
-    content_tag :span, text, :class => 'badge' + tags.map { |tag| ' badge-' + tag.to_s }.join
+  def badge_tag(type, body, options = {})
+    content_tag :span, body, options.merge(class: "badge badge-#{type.to_s}")
   end
 
-  def icon_tag(*tags)
-    content_tag :i, nil, :class => tags.map { |tag| 'icon-' + tag.to_s }.join(' ')
+  def icon_tag(type, body = nil, options = {})
+    icon = content_tag :i, nil, options.merge(class: "icon-#{type.to_s}")
+    
+    unless body.blank?
+      body = options[:append].blank? ? (icon + body.to_s) : (body.to_s + icon)
+      icon = content_tag :span, body.html_safe, options      
+    end
+    
+    icon
   end
   
   def time_tag(time, format = :long_ordinal, options = {})
@@ -17,7 +24,11 @@ module TagHelper
      link_to_with_tooltip body, '#', tip, options
   end
   
+  # TODO rm?
   def link_to_with_tooltip(body, url, tip = body, options = {})
     link_to body, url, options.merge({ rel: :tooltip, title: tip })
+  end
+  
+  def link_to_with_icon( icon)
   end
 end
