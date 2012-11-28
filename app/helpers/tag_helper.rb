f16746876537cc393edda4e5007d2f20affa95c3
Pavol Zbell <pavol.zbell@gmail.com>
2012-11-28 19:11:06 +0100

    Refactor footer

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -10,10 +10,14 @@ module TagHelper
   def time_tag(time, format = :long_ordinal, options = {})
     options[:class] ||= 'timeago'
     
-    content_tag :abbr, time.to_formatted_s(format), options.merge(:title => time.getutc.iso8601)
+    content_tag :abbr, time.to_formatted_s(format), options.merge(title: time.getutc.iso8601)
   end
 
-  def tooltip_tag(text, tooltip, options = {})
-    link_to text, '#', options.merge({ :rel => :tooltip, :title => tooltip }) 
-  end  
+  def tooltip_tag(body, tip, options = {})
+     link_to_with_tooltip body, '#', tip, options
+  end
+  
+  def link_to_with_tooltip(body, url, tip = body, options = {})
+    link_to body, url, options.merge({ rel: :tooltip, title: tip })
+  end
 end
