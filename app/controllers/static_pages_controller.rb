399ac8256fada621a390efc2ba8599c9d757c0ba
Pavol Zbell <pavol.zbell@gmail.com>
2012-12-13 00:30:05 +0100

    Revert home page counter to normal

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,8 +6,6 @@ class StaticPagesController < ApplicationController
   end
   
   def home
-    # TODO rm
-    @count = 412569
-    #@count = Hearing.count + Decree.count
+    @count = Hearing.count + Decree.count
   end
 end
