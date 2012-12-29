760cf16442a5431c7b847af4ae1fba028e22a777
Pavol Zbell <pavol.zbell@gmail.com>
2012-12-30 00:49:50 +0100

    Remove unused methods from String class

--- lib/ruby/string.rb
+++ lib/ruby/string.rb
@@ -24,20 +24,4 @@ class String
   def upcase_first!
     self.sub!(/^\D{0,1}/) { |c| c.upcase }
   end
-  
-  def underscore
-    self.gsub(/::/, '/')
-        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-        .tr('-', '_')
-        .downcase
-  end
-  
-  def underscore!
-    self.gsub!(/::/, '/')
-        .gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-        .gsub!(/([a-z\d])([A-Z])/, '\1_\2')
-        .tr!('-', '_')
-        .downcase!
-  end
 end
