1568288abad5be7f30400df87ef2e010f2a09f0d
Pavol Zbell <pavol.zbell@gmail.com>
2012-12-31 04:05:48 +0100

    Remove old PostgreSQL template

--- db/template.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE template_sk_utf8_ci 
-	WITH 
-	  TEMPLATE      = template0 
-	  ENCODING      = 'UNICODE'
-	  LC_COLLATE    = 'sk_SK.utf8'
-	  LC_CTYPE      = 'sk_SK.utf8';
-
-UPDATE pg_database SET datistemplate = true WHERE datname = 'template_sk_utf8_ci';
-
