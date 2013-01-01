ce23a21e1a33534876a5998667cf6b2a953ddf6d
Pavol Zbell <pavol.zbell@gmail.com>
2013-01-01 22:29:32 +0100

    Refactor enumerable type models

--- app/models/type/enumerable.rb
+++ app/models/type/enumerable.rb
@@ -2,17 +2,27 @@ module Type::Enumerable
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def value(name, value)
-      define_singleton_method name do
-        @values = {} unless @values
-        
-        unless @values[name]
-          @values[name] = self.find_or_create_by_value(value)
-          @values[name].save!
-        end 
+    def values
+      @values || {}
+    end
 
-        @values[name]
+    def value(name, value)
+      @values = {} unless @values
+
+      unless @values[name]
+        @values[name] = self.find_or_create_by_value(value)
+        @values[name].save!
       end
+      
+      define_singleton_method name do
+        @values[name]
+      end      
+    end
+  end
+  
+  module InstanceMethods
+    def name
+      self.class.values.invert[self]
     end
   end
 end
