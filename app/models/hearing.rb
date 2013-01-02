1ea80348e9e81a31aeac6848e94e5009ebe08f80
Samuel Molnar <molnar.samuel@gmail.com>
2013-01-02 19:24:25 +0100

    Add elasticsearch index for Hearing

--- app/models/hearing.rb
+++ app/models/hearing.rb
@@ -10,6 +10,9 @@ class Hearing < ActiveRecord::Base
                   :chair_judge_name_unprocessed,
                   :selfjudge,
                   :note
+
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
   
   belongs_to :proceeding
   
@@ -29,4 +32,44 @@ class Hearing < ActiveRecord::Base
   has_many :proposers,  dependent: :destroy
   has_many :opponents,  dependent: :destroy
   has_many :defendants, dependent: :destroy
+
+
+  settings analysis: { 
+            filter: {
+              name_ngram: {
+                'type'      => 'NGram',
+                'min_gram'  => 3,
+                'max_gram'  => 12
+              }
+            },
+            analyzer: {
+              name_analyzer: {
+                'type'      => 'custom',
+                'tokenizer' => 'standard',
+                'filter'    => %w(lowercase asciifolding name_ngram)
+              }
+            }
+  }
+
+  mapping do
+    indexes :id
+    indexes :case_number 
+    indexes :file_number
+    indexes :date
+    indexes :room
+    indexes :special_type
+    indexes :commencement_date
+
+    indexes :type,              as: lambda { |h| h.section.value                     }
+    indexes :subject,           as: lambda { |h| h.subject.value                     }
+    indexes :form,              as: lambda { |h| h.form.value                        }
+    indexes :judges,            as: lambda { |h| h.judgings.map { |j| j.judge.name } }, 
+                                analyzer: 'name_analyzer'
+    indexes :proposers,         as: lambda { |h| h.proposers.map { |p| p.name }      },
+                                analyzer: 'name_analyzer'
+    indexes :opponents,         as: lambda { |h| h.opponents.map { |o| o.name }      },
+                                analyzer: 'name_analyzer'
+    indexes :defendants,        as: lambda { |h| h.defendants.map { |d| d.name }     },
+                                analyzer: 'name_analyzer'
+  end
 end
