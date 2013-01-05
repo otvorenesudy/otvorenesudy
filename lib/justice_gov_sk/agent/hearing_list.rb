5b3d6812bc0b3d5da9434fba38837a976274b5fd
Samuel Molnar <molnar.samuel@gmail.com>
2013-01-05 13:25:17 +0100

    Fix bug in HearingList agent when including old records

--- lib/justice_gov_sk/agent/hearing_list.rb
+++ lib/justice_gov_sk/agent/hearing_list.rb
@@ -3,13 +3,18 @@ module JusticeGovSk
     class HearingList < JusticeGovSk::Agent::List
       def download(request)
         super(request) do |page|
-          form, fields = form_and_fields(page, form_name)
+          form, _ = form_and_fields(page, form_name)
 
           # Include old hearings, only for hearings
           if request.respond_to?(:include_old_hearings) && request.include_old_hearings
             checkboxes = form.checkboxes.map(&:name)
             include_old_hearings_checkbox_name = checkboxes.find { |f| f.match(/\A.+StarsiePojednavania\z/) }
+            
+            form.checkbox_with(:name => include_old_hearings_checkbox_name).check
+            page = form.submit
           end
+
+          page
         end
       end
     end
