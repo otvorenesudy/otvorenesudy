240ededbd4a49076f991942e2c8def86ec553aff
Samuel Molnar <molnar.samuel@gmail.com>
2013-01-05 14:10:18 +0100

    Refactor TextExtractor tests

--- spec/core/text_extractor_spec.rb
+++ spec/core/text_extractor_spec.rb
@@ -1,23 +1,22 @@
 require 'spec_helper'
 
-describe TextExtractor do 
+describe Core::TextExtractor do 
   it "should extract text from pdf file" do
     file = File.join(FIXTURES, "documents", "good.pdf")
 
-    @ext = TextExtractor.new
+    @ext = Core::TextExtractor.new
     content = @ext.extract(file)
 
     content.starts_with?("A Circle Drawing Algorithm").should be_true
-    content.length.should == 3719
+    content.length.should == 3835
   end
 
   it "should extract nothing from pdf file with scanned pages" do
     file = File.join(FIXTURES, "documents", "bad.pdf")
 
-    @ext = TextExtractor.new
+    @ext = Core::TextExtractor.new
     content = @ext.extract(file)
     
     content.strip.should be_empty
    end
 end
-
