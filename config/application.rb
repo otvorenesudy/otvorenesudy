a672beba5a143ae30225b2332f19d9367271295d
Pavol Zbell <pavol.zbell@gmail.com>
2013-01-28 21:06:13 +0100

    Refactor loading of custom libraries

--- config/application.rb
+++ config/application.rb
@@ -27,8 +27,8 @@ module OpenCourts
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
-    # Include all library directories.
-    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    # Include library root.
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
--- config/initializers/justice_gov_sk.rb
+++
@@ -1,1 +0,0 @@
-require 'justice_gov_sk'
