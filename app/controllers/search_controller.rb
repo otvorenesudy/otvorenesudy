776c4a807aa5947da8e3ff03e8520e8d3aa325e4
Samuel Molnar <molnar.samuel@gmail.com>
2013-02-03 19:53:14 +0100

    Add autocompletition sample.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,11 @@
 class SearchController < ApplicationController
+
+  def autocomplete
+    @models = %w(Judge)
+    
+    render json: {
+      data: Judge.first(5).shuffle.map { |e| { value: e.name, facet: '20' } }
+    }
+  end
+
 end
