f62065e4122522af3ed89738571774c1abed5422
Samuel Molnar <molnar.samuel@gmail.com>
2013-02-04 15:25:06 +0100

    Add helper for search view.

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,21 @@
 module SearchHelper
+
+  def category_title(title)
+    content_tag :div, title, class: 'title'     
+  end
+
+  def category_input(options)
+    content_tag :input, nil, 
+      id:          options[:id], 
+      name:        'input',
+      type:        'text',
+      placeholder: options[:placeholder]
+  end
+
+  def category_list(options)
+    content_tag :ul, nil,
+      id:    "#{options[:id]}-list",
+      class: 'unstyled'
+  end
+  
 end
