380d53f093026a9ed06becfac61d34ba350d2595
Pavol Zbell <pavol.zbell@gmail.com>
2013-02-04 22:46:55 +0100

    Implement prototype view of hearing and decree

--- app/helpers/decrees_helper.rb
+++ app/helpers/decrees_helper.rb
@@ -1,4 +1,8 @@
 module DecreesHelper
+  def decree_date(date)
+    time_tag date, format: :long 
+  end
+
   def link_to_decree(decree, options = {})
     link_to decree.file_number, decree_path(decree.id), options
   end
