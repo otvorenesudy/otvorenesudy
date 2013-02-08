ac4991706eecdbff3c5f90e4214efeb0b219a912
Samuel Molnar <molnar.samuel@gmail.com>
2013-02-08 23:47:26 +0100

    Add search method to SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,13 +13,43 @@ class SearchController < ApplicationController
     else
       render status: 422,
       json: {
-        error: "#{params[:entity]} is not valid autocomplete entity, damn!"
+        error: "#{params[:entity]} is not valid autocomplete entity!"
       }
     end
   end
 
   def search
+    @model, @query = parse_search_query
+    
+    @query[:facets] = [:judges]
+    
+    result = @model.search_by(@query)
+    
+    render json: {
+      data: result[:results],
+      facets: result[:facets]
+    }
+  end
+    
+  private 
 
+  def parse_search_query
+
+    model = Hearing
+    query = Hash.new
+    
+    params.each do |key, value|
+      
+      case key
+      when :category
+        # TODO: Parse category
+      when :judges
+        query[:filter].merge! judges: params[key] if params[key].is_a?(Array)
+      end
+
+    end
+
+    return model, query
   end
 
 end
