70a888586f27e58f8bdb7002f3be0b1d086372ba
Samuel Molnar <molnar.samuel@gmail.com>
2013-02-08 23:48:23 +0100

    Refactor and fix search module.

--- app/models/document/searchable.rb
+++ app/models/document/searchable.rb
@@ -7,52 +7,115 @@ module Document
 
     module ClassMethods
 
-      # TODO: hard refactoring and handle sorting
-      def _search(params)
-        params[:page]   ||= 1
-        params[:per_page] = 10
+      def search_by(params)
+        format_result(compose_search(params))
+      end
 
-        query = params[:search][:query]  || {}
-        terms = params[:search][:filter] || {}
+      private 
 
-        facets = params[:search][:facets]
+      def fetch_records(hits)
+        return [] unless hits
 
-        tire.search do
-          query do
-            boolean do 
-              query.each do |field, values|
+        hits.map do |hit|
+          self.find(hit.id)
+        end
+      end
 
-                case 
-                when values.is_a?(Range)
+      def format_facets(facets)
+        result = Hash.new
 
-                  must { range field, { gte: values.min, lte: values.max } }
+        return result unless facets
 
-                else
+        facets.each do |field, values|
+          result[field.to_sym] = values['terms'].map { |e| e.symbolize_keys } 
+        end
 
-                  must { string "*#{values}*", default_field: "#{field}.analyzed", default_operator: :and, analyze_wildcard: true }
+        result
+      end
+     
+      def format_result(result)
+        data = Hash.new
+
+        data[:results] = fetch_records(result.results)
+        data[:facets]  = format_facets(result.facets)
+
+        data
+      end
+
+      def compose_search(params)
+        puts params.inspect
+
+        page       = params[:page]      || 1
+        per_page   = params[:per_page]  || 10
+        query      = params[:query]     || Hash.new
+        terms      = params[:filter]    || Hash.new
+        facets     = params[:facets]
+        sort_block = params[:sort]
+
+        results = tire.search do
+
+          if query.any? or terms.any?
+            query do
+              boolean do
+                query.each do |field, values|
+
+                  case 
+                  when values.is_a?(Range)
+
+                    must { range field, { gte: values.min, lte: values.max } }
+
+                  else
+
+                    must { string "*#{values}*", default_field: "#{field}.analyzed", default_operator: :and, analyze_wildcard: true  }
+
+                  end
 
                 end
-              end
 
-              terms.each do |field, values|
-                must { term "#{field}.untouched", values }
+                terms.each do |field, value|
+                  field = "#{field}.untouched"
+
+                  if value.respond_to?(:each)
+
+                    value.each do |e|
+                      must { term field, e }
+                    end
+
+                  else
+
+                    must { term field, value }
+
+                  end
+
+                end
               end
             end
           end
 
           if facets.respond_to?(:each)
+
             facets.each do |field|
+
               facet field.to_s do 
                 terms "#{field}.untouched"
               end
+
             end
+
           elsif facets
+
             facet facets.to_s do
               terms "#{facets}.untouched"
             end
+
           end
 
+          if sort_block
+            sort sort_block
+          end
         end
+
+        results
       end
     end
   end
