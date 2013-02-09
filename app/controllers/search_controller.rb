92b2067d7cd4c502b372adc16ef3a78def4cd18b
Samuel Molnar <molnar.samuel@gmail.com>
2013-02-09 02:23:16 +0100

    Fix few bugs in SearchController.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,16 @@
 class SearchController < ApplicationController
 
   def autocomplete
-    @whitelist = [:judges]
+    @whitelist = [:judges, :court]
 
-    entity = params[:entity].to_sym
-    term   = params[:term]
+    @entity = params[:entity].to_sym
+    @term   = params[:term]
 
-    if @whitelist.include?(entity)
+    if @whitelist.include?(@entity)
+      @model, @query = parse_search_query(params[:data])
+
       render json: {
-        data: Hearing.suggest(entity, term)   
+        data: @model.suggest(@entity, @term, @query)
       }
     else
       render status: 422,
@@ -19,32 +21,36 @@ class SearchController < ApplicationController
   end
 
   def search
-    @model, @query = parse_search_query
+    @model, @query = parse_search_query(params[:data])
     
-    @query[:facets] = [:judges]
+    @query[:facets] = [:judges, :court]
     
-    result = @model.search_by(@query)
+    result, total_count = @model.search_by(@query)
     
     render json: {
-      data: result[:results],
+      data: render_to_string(:partial => 'results', locals: { type: @model, values: result[:results], options: { total: total_count } }),
       facets: result[:facets]
     }
   end
     
   private 
 
-  def parse_search_query
+  def parse_search_query(params)
 
     model = Hearing
     query = Hash.new
+
+    query[:filter] = Hash.new
     
-    params.each do |key, value|
-      
+    params.symbolize_keys.each do |key, value|
+
       case key
       when :category
         # TODO: Parse category
       when :judges
-        query[:filter].merge! judges: params[key] if params[key].is_a?(Array)
+        query[:filter].merge!(judges: params[key]) if params[key].is_a?(Array)
+      when :court
+        query[:filter].merge!(court: params[key]) if params[key].is_a?(Array)
       end
 
     end
