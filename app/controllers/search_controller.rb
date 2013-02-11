7fc97b9680ae02297fc24c9e35c23a5dd9a620e0
Pavol Zbell <pavol.zbell@gmail.com>
2013-02-11 21:04:36 +0100

    Attempt to refactor search controller, add notes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -19,6 +19,8 @@ class SearchController < ApplicationController
   end
 
   def search
+    # TODO refactor -- name variables more carefully
+    
     @model, @query = parse_search_query(params[:data])
     
     @query[:facets]  = [:judges, :court]
@@ -26,15 +28,19 @@ class SearchController < ApplicationController
     
     @search, @results = @model.search_by(@query)
     
+    
+    
     render json: {
-      data: render_to_string(partial: 'results', 
+      facets: @search[:facets],
+      data: render_to_string(partial: 'results', # TODO rename 'data' key to 'results' 
                              locals: { 
                                 type: @model, 
                                 values: @search[:results], 
                                 options: { results: @results } 
                             }
       ),
-      facets: @search[:facets]
+      pagination: render_to_string(partial: 'pagination', locals: { results: @results }), # TODO bind to its own HTML id
+      #TODO add info: -> locals: { count: 61559 } -> search/_info.html.erb -> "Celkovo najdených 61559 súdnych dokumentov", bind to its own HTML id
     }
   end
     
