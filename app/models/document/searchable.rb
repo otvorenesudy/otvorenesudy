15c1ac7bc859e919b82eb496f1089e971cedab05
Samuel Molnar <molnar.samuel@gmail.com>
2013-02-12 23:39:42 +0100

    Partially refactor Searchable module.

--- app/models/document/searchable.rb
+++ app/models/document/searchable.rb
@@ -44,95 +44,111 @@ module Document
         return data, result
       end
 
-      def compose_search(params)
+      def compose_search(params, &block)
         page       = params[:page]      || 1
         per_page   = params[:per_page]  || 10
         query      = params[:query]     || Hash.new
         terms      = params[:filter]    || Hash.new
         facets     = params[:facets]    || []
-        sort_block = params[:sort]
         options    = params[:options]   || {}
+        sort_block = params[:sort]
 
         options[:global_facets] ||= false
 
         facets = [facets] unless facets.respond_to?(:each)
 
-        results = tire.search page: page, per_page: per_page do
+        results = tire.search page: page, per_page: per_page do |index|
 
-          if query.any? or terms.any?
-            query do
-              boolean do
-                query.each do |field, values|
-
-                  case 
-                  when values.is_a?(Range)
-
-                    must { range field, { gte: values.min, lte: values.max } }
-
-                  else
-
-                    must { string "*#{values}*", default_field: "#{field}.analyzed", default_operator: :and, analyze_wildcard: true  }
-
-                  end
-
-                end
-
-                terms.each do |field, value|
-                  field = "#{field}.untouched"
-
-                  if value.respond_to?(:each)
-
-                    value.each do |e|
-                      must { term field, e }
-                    end
-
-                  else
-
-                    must { term field, value }
-
-                  end
-
-                end
-              end
-            end
+          if block
+            block.call(index)
+          else
+            query(index, query, terms, options)
+            facets(index, facets, terms, options)
+            sort(index, sort_block, options)
           end
 
-          if facets.any?
-            facets.each do |facet|
-
-              facet facet.to_s, global: options[:global_facets] do 
-                terms "#{facet}.untouched"
-
-                if terms.any?
-                  filter_values = []
- 
-                  terms.except(facet).each do |field, value|
-                    if value.respond_to?(:each)
-                     
-                      value.each do |e|
-                        filter_values << { term: { "#{field}.untouched" => e } }
-                      end
-
-                    else
-                      
-                      filter_values << { term: { "#{field}.untouched" => value } }
-
-                    end
-                  end
-
-                  facet_filter :and, filter_values if filter_values.any?
-                end
-              end
-            end
-          end
-
-          if sort_block
-            sort sort_block
-          end
         end
 
         results
       end
+
+      def query(index, query, terms, options)
+        if query.any? or terms.any?
+          index.query do
+            boolean do
+              query.each do |field, values|
+
+                case 
+                when values.is_a?(Range)
+
+                  must { range field, { gte: values.min, lte: values.max } }
+
+                else
+
+                  must { string "*#{values}*", default_field: "#{field}.analyzed", default_operator: :and, analyze_wildcard: true  }
+
+                end
+
+              end
+
+              terms.each do |field, value|
+                field = "#{field}.untouched"
+
+                if value.respond_to?(:each)
+
+                  value.each do |e|
+                    must { term field, e }
+                  end
+
+                else
+
+                  must { term field, value }
+
+                end
+
+              end
+            end
+          end
+        end
+      end
+
+      def facets(index, facets, terms, options)
+        if facets.any?
+          facets.each do |facet|
+
+            index.facet facet.to_s, global: options[:global_facets] do 
+              terms "#{facet}.untouched"
+
+              if terms.any?
+                filter_values = []
+
+                terms.except(facet).each do |field, value|
+                  if value.respond_to?(:each)
+
+                    value.each do |e|
+                      filter_values << { term: { "#{field}.untouched" => e } }
+                    end
+
+                  else
+
+                    filter_values << { term: { "#{field}.untouched" => value } }
+
+                  end
+                end
+
+                facet_filter :and, filter_values if filter_values.any?
+              end
+            end
+          end
+        end
+      end
+
+      def sort(index, sort_block, options)
+        if sort_block
+          index.sort sort_block
+        end
+      end
+
     end
   end
 end
