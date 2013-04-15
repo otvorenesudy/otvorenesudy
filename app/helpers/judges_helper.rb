edce9046edbedf15f048e0011d147acc1404f50b
Samuel Molnar <molnar.samuel@gmail.com>
2013-04-15 02:01:31 +0200

    Propose solution for partial judge view

--- app/helpers/judges_helper.rb
+++ app/helpers/judges_helper.rb
@@ -11,10 +11,10 @@ module JudgesHelper
 
   def judge_position(employment)
     options = employment.active ? {} : { class: :muted }
-    
+
     tooltip_tag employment.judge_position.value, employment.active ? 'Aktívny' : 'Neaktívny', :left, :hover, options
   end
-  
+
   def judge_at_court_employment(judge, court)
     judge.employments.at_court(court).first
   end
@@ -26,14 +26,14 @@ module JudgesHelper
   def judge_hearings_count_by_employment(employment, options = {})
     content_tag :span, Hearing.during_employment(employment).count, employment.active ? {} : { class: :muted }
   end
-  
+
   def judge_decrees_count_by_employment(employment, options = {})
     content_tag :span, Decree.during_employment(employment).count, employment.active ? {} : { class: :muted }
   end
-      
+
   def judge_name(judge, format = nil)
     return judge.name if format.nil? || format == '%p %f %m %l %a, %s'
-    
+
     parts = {
       '%p' => judge.prefix,
       '%f' => judge.first,
@@ -42,26 +42,36 @@ module JudgesHelper
       '%s' => judge.suffix,
       '%a' => judge.addition
     }
-    
+
     format.gsub(/\%[pfmlsa]/, parts).gsub(/(\W)\s+\z/, '').squeeze(' ')
   end
-  
+
   def judge_titles(judge, options = {})
     content_tag :span, "#{judge.prefix} #{judge.suffix}".strip, judge_options(judge, options)
   end
-  
+
   def link_to_judge(judge, options = {})
     link_to judge_name(judge, options.delete(:format)), judge_path(judge.id), judge_options(judge, options)
   end
-  
+
   def links_to_judges(judges, options = {})
     separator = options.delete(:separator) || ', '
-    
-    judges.map { |judge| link_to_judge judge, options }.join(separator).html_safe
+
+    judges.map { |judge| judge.respond_to?(:id) ? link_to_judge(judge, options) : link_to_partial_judge(judge, options) }.join(separator).html_safe
+  end
+
+  def link_to_partial_judge(name, options = {})
+    tooltip_tag name, 'Sudca nie je uvedený v zozname sudcov.', :bottom, :hover, options
+  end
+
+  def links_to_partial_judges(judges, options = {})
+    separator = options.delete(:separator) || ', '
+
+    judges.map { |judge| link_to_partial_judge judge, options }.join(separator)
   end
 
   private
-  
+
   def judge_options(judge, options)
     options.merge! judge.active ? {} : { class: :muted } if options.delete :adjust_by_activity
     options.merge! judge.active_at(options.delete :adjust_by_activity_at) ? {} : { class: :muted } if options.key? :adjust_by_activity_at
