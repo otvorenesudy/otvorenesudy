5c044f0166a276c8e979fbadf4c6d95bc5db9700
Pavol Zbell <pavol.zbell@gmail.com>
2013-04-16 17:17:33 +0200

    Add wierd decree date warnings

--- app/controllers/decrees_controller.rb
+++ app/controllers/decrees_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class DecreesController < ApplicationController
   def show
     @decree = Decree.find(params[:id])
@@ -5,5 +7,8 @@ class DecreesController < ApplicationController
     @court = @decree.court
     
     @legislations = @decree.legislations.order(:value)
+    
+    flash[:error]  = render_to_string(partial: 'date_error').html_safe  if @decree.date > Time.now.to_date
+    flash[:notice] = render_to_string(partial: 'date_notice').html_safe if @decree.date > @decree.created_at.to_date
   end
 end
