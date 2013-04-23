74f928288ae6a4f3ee7306a71d5ddb200d815f8a
Pavol Zbell <pavol.zbell@gmail.com>
2013-04-24 02:40:17 +0300

    Format search facet model, use correct OR operator

--- app/models/document/search/facet.rb
+++ app/models/document/search/facet.rb
@@ -1,17 +1,14 @@
 module Document
   module Search
     module Facet
-
       def build_facet(index, name, field, facet, options)
         index.facet name, options do |f|
-
           facet.build(f, not_analyzed_field(field))
-
         end
       end
 
       def facet_filter(query, facets)
-        filter = build_facet_filter(query, facets) if query.any? or facets.any?
+        filter = build_facet_filter(query, facets) if query.any? || facets.any?
 
         { and: filter } if filter.any?
       end
@@ -19,7 +16,6 @@ module Document
       def build_facet_filter(query, facets)
         build_query(query).concat(build_filter(:or, facets))
       end
-
     end
   end
 end
