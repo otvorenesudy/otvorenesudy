ff1945b3ea34d0dc2ea645b85389bfb9089a6123
Pavol Zbell <pavol.zbell@gmail.com>
2013-05-14 17:10:32 +0200

    Fix bugs in Source model

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,11 +1,9 @@
 class Source < ActiveRecord::Base
   attr_accessible :module,
                   :name,
-                  :data_uri,
-                  :site_uri
+                  :uri
 
   validates :module, presence: true
   validates :name,   presence: true
-  
-  validates :data_uri, presence: true
+  validates :uri,    presence: true
 end
