be48b36830730c3d4e21408e71016995f50b530b
Pavol Zbell <pavol.zbell@gmail.com>
2013-05-22 17:44:28 +0200

    Make decree notice and error partial names more concrete

--- app/controllers/decrees_controller.rb
+++ app/controllers/decrees_controller.rb
@@ -8,7 +8,7 @@ class DecreesController < ApplicationController
     
     @legislations = @decree.legislations.order(:value)
     
-    flash.now[:notice] = render_to_string(partial: 'date_notice').html_safe if @decree.had_future_date?
-    flash.now[:error]  = render_to_string(partial: 'date_error').html_safe  if @decree.has_future_date?
+    flash.now[:notice] = render_to_string(partial: 'has_future_date_notice').html_safe if @decree.had_future_date?
+    flash.now[:error]  = render_to_string(partial: 'had_future_date_error').html_safe  if @decree.has_future_date?
   end
 end
