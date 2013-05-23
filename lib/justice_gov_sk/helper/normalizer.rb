3fc13e6ae27ab067251254a22e50da1b36a5d021
Samuel Molnar <molnar.samuel@gmail.com>
2013-05-23 03:29:16 +0200

    Update normalizer

--- lib/justice_gov_sk/helper/normalizer.rb
+++ lib/justice_gov_sk/helper/normalizer.rb
@@ -9,9 +9,9 @@ module JusticeGovSk
         value.strip!
         value.gsub!(/\-/, '')
         value.squeeze!(' ')
-        
+
         key = value.ascii.downcase
-  
+
         court_name_map[key] || (value.utf8.split(/\s+/).map { |part|
           if !part.match(/\A(I|V)+\z/).nil?
             part
@@ -22,17 +22,18 @@ module JusticeGovSk
           end
         }.join ' ')
       end
-      
+
       private
-      
+
       def court_name_map
         return @court_name_map if @court_name_map
-        
+
         map = {
           "Najvyšší súd SR"                => "Najvyšší súd Slovenskej republiky",
+          "Najvyšší súd"                   => "Najvyšší súd Slovenskej republiky",
           "Ústavný súd SR"                 => "Ústavný súd Slovenskej republiky",
           "Špecializovaný trestný súd"     => "Špecializovaný trestný súd",
-          
+
           "Krajský súd v Banskej Bystrici" => "Krajský súd Banská Bystrica",
           "Krajský súd v Bratislave"       => "Krajský súd Bratislava",
           "Krajský súd v Košiciach"        => "Krajský súd Košice", 
@@ -41,35 +42,37 @@ module JusticeGovSk
           "Krajský súd v Trenčíne"         => "Krajský súd Trenčín",
           "Krajský súd v Trnave"           => "Krajský súd Trnava",
           "Krajský súd v Žiline"           => "Krajský súd Žilina",
-          
+
           "Okresný súd Bánovce n/B"        => "Okresný súd Bánovce nad Bebravou",
-          "Okresný súd Nové Mesto n/V"     => "Okresný súd Nové Mesto nad Váhom"
+          "Okresný súd Nové Mesto n/V"     => "Okresný súd Nové Mesto nad Váhom",
+          "Okresný súd Žiar n/H"           => "Okresný súd Žiar nad Hronom",
+          "Okresný súd Vranov n/T"         => "Okresný súd Vranov nad Topľou"
         }
-        
+
         @court_name_map = {}
-        
+
         map.each { |k, v| @court_name_map[k.ascii.downcase] = v }
-        
+
         @court_name_map 
       end
-      
+
       public
-      
+
       def normalize_municipality_name(value)
         value.strip!
-        
+
         value == "Bratislava 33" ? "Bratislava III" : value
       end
-  
+
       def normalize_person_name(value)
         partition_person_name(value)[:altogether]
       end
-      
+
       # TODO refactor, method too large -> focus on titles
       def partition_person_name(value)
         copy  = value.clone
         value = value.utf8
-        
+
         prefixes  = []
         suffixes  = []
         additions = []
@@ -78,15 +81,15 @@ module JusticeGovSk
         flags     = []
 
         _, special = *value.match(/((\,\s+)?hovorca)?\s+KS\s+(v\s+)?(?<municipality>.+)\z/i) 
-        
+
         unless special.nil?
           flags << :special
           value.sub!(/((\,\s+)?hovorca\s+)?KS\s+(v\s+)?.+\z/i, '')
         end
-        
+
         value.strip.gsub(/[\,\;]/, '').split(/\s+/).each do |part|
           part = part.utf8.squeeze('.').strip
-          
+
           if part.match(/\./)
             if part.match(/prom\.\správ\./i)
               prefixes << "prom. práv."
@@ -100,7 +103,7 @@ module JusticeGovSk
               additions << part.gsub(/[\(\)]/, '')
             else
               part.downcase! if part.match(/(akad|arch|art|doc|prof)\./i)
-              
+
               prefixes << part
             end
           else
@@ -115,28 +118,28 @@ module JusticeGovSk
             end
           end
         end
-        
+
         names = mixedcase + uppercase
-        
+
         if flags.include? :born
           names << names.last
           names[-2] = "rod."
         end
-        
+
         value = nil
         value = names.join(' ')                   unless names.empty?
         value = prefixes.join(' ') + ' ' + value  unless prefixes.empty?
         value = value + ' ' + additions.join(' ') unless additions.empty?
         value = value + ', ' + suffixes.join(' ') unless suffixes.empty?
-        
+
         if flags.include? :special
           municipality ||= "Trenčíne" unless special.match(/(TN|Trenčín(e)?)/).nil?
           municipality ||= "Trnave"   unless special.match(/(TT|Trnav(a|e){1})/).nil?
-          
+
           role  = "Hovorca krajského súdu v #{municipality}"
           value = value.blank? ? role : "#{value}, #{role.downcase_first}"
         end
-        
+
         {
           unprocessed: copy.strip,
           altogether:  value,
@@ -149,48 +152,48 @@ module JusticeGovSk
           role:        role
         }
       end
-      
+
       private
-      
+
       def person_name_title_map
         @person_name_title_map if @person_name_title_map
-        
+
         map = {}
-        
+
         ['JUDr', 'Mgr', 'PhD', 'CSc', 'DrSc'].each { |v| map[v.downcase.to_sym] = v }
-        
+
         @person_name_title_map = map
       end
-      
+
       public
-      
+
       def normalize_zipcode(value)
         value = value.ascii.strip.gsub(/\s+/, '')
-        
+
         "#{value[0..2]} #{value[3..-1]}"
       end
-      
+
       def normalize_street(value)
         value = value.utf8
-        
+
         value.gsub!(/\,/, ' ')
         value.gsub!(/sv\./i, 'sv. ')
         value.gsub!(/slov\./i, 'slovenskej')
-        
+
         value.gsub!(/Námestie/i) { |s| "#{s[0]}ám." }
         value.gsub!(/Ulica/i)    { |s| "#{s[0]}l." }
         value.gsub!(/Číslo/i)    { |s| "#{s[0]}." }
-        
+
         value.strip!
         value.squeeze!(' ')
-        
+
         value
       end
-      
+
       def normalize_email(value)
         value.split(/\,|\;/).map { |part| part.strip }.join ', '
       end
-   
+
         # TODO impl   
       def normalize_phone(value)
         value.strip
@@ -210,47 +213,47 @@ module JusticeGovSk
   #          end
   #        end.join ', '
       end
-      
+
       def normalize_hours(value)
         value = value.ascii.gsub(/[a-z]+/i, '')
-        
+
         times = value.split(/\s*\-\s*|\,\s*|\;\s*|\s+/).map do |time|
           hour, minute = time.split(/\:/)
           "#{'%d' % hour.to_i}:#{'%02d' % minute.to_i}"
         end
-        
+
         times.each_slice(2).map { |interval| "#{interval.first} - #{interval.last}" }.join ', '
       end
-  
+
       def normalize_date(value)
         _, day, month, year = *value.strip.match(/(\d+)\.(\d+)\.(\d+)?/)
-  
+
         "#{'%04d' % year.to_i}-#{'%02d' % month.to_i}-#{'%02d' % day.to_i}"
       end
-      
+
       def normalize_datetime(value)
         _, day, month, year, hour, minute, second = *value.strip.match(/(\d+)\.(\d+)\.(\d+)\s+(\d+)\.(\d+)(\.(\d+))?/)
-  
+
         hour   = 0 if hour.nil?
         minute = 0 if minute.nil?
         second = 0 if second.nil?
-        
+
         date = "#{'%04d' % year.to_i}-#{'%02d' % month.to_i}-#{'%02d' % day.to_i}"
         time = "#{'%02d' % hour.to_i}:#{'%02d' % minute.to_i}:#{'%02d' % second.to_i}"
-        
+
         "#{date} #{time}"
       end
-      
+
       def normalize_punctuation(value)
         value.gsub!(/\s*[\.\,\;]/) { |s| "#{s[-1]} " }
         value.gsub!(/[\.\,\;]\s+[\.\,\;]/) { |s| "#{s[0]}#{s[-1]}" }
-        
+
         value.gsub!(/(s\s*\.\s*r\s*\.\s*o|k\s*\.\s*s|a\s*\.\s*s|o\s*\.\s*z|n\s*\.\s*o)\s*\.?/i) do |s|
           "#{s.gsub(/\s/, '').downcase}#{s[-1] == '.' ? '' : '.'}"
         end
-        
+
         value.gsub!(/-/, ' - ')
-        
+
         value.strip.squeeze(' ')
       end
     end
