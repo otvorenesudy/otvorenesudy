1ae91468efaa88070001d2a756456e53cdbdba19
Samuel Molnar <molnar.samuel@gmail.com>
2013-05-23 14:26:57 +0200

    Add source and uri to JudgeDesignation

--- app/models/judge_designation.rb
+++ app/models/judge_designation.rb
@@ -1,9 +1,10 @@
 class JudgeDesignation < ActiveRecord::Base
-  attr_accessible :date
+  attr_accessible :date, :uri
 
   belongs_to :judge
-  
-  belongs_to :type, class_name: :JudgeDesignationType, foreign_key: :judge_designation_type_id
+  belongs_to :source
+  belongs_to :type,  class_name: :JudgeDesignationType, foreign_key: :judge_designation_type_id
 
   validates :date, presence: true
+  validates :uri,  presence: true
 end
--- db/migrate/20130516184433_create_judge_designations.rb
+++ db/migrate/20130516184433_create_judge_designations.rb
@@ -3,12 +3,18 @@ class CreateJudgeDesignations < ActiveRecord::Migration
     create_table :judge_designations do |t|
       t.references :judge,                  null: false
       t.references :judge_designation_type, null: true
-      
-      t.date :date, null: false
+      t.references :source,                 null: false
+
+      t.date   :date, null: false
+      t.string :uri,  null: false
 
       t.timestamps
     end
 
     add_index :judge_designations, :judge_id
+    add_index :judge_designations, :judge_designation_type_id
+    add_index :judge_designations, :source_id
+    add_index :judge_designations, :uri
+    add_index :judge_designations, :date
   end
 end
