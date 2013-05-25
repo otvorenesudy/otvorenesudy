abe9ae73a47e10fbb68cc4fb49b3df7222b7f9bb
Pavol Zbell <pavol.zbell@gmail.com>
2013-05-25 16:21:29 +0200

    Use date distance in judge designation view

--- app/helpers/judges_helper.rb
+++ app/helpers/judges_helper.rb
@@ -84,16 +84,8 @@ module JudgesHelper
     content_tag :span, number_with_delimiter(Decree.during_employment(employment).count), employment.active ? {} : { class: :muted }
   end
 
-    # TODO rm
-#    
-  def judge_designation_distance(designation)
+  def judge_designation_date_distance(designation)
     tooltip_tag distance_of_time_in_words_to_now(designation.date), I18n.l(designation.date, format: :long)
-  end
-  
-  def judge_designation_date(designation, options = {})
-    return time_tag(designation.date) unless designation.type
-
-    tooltip_tag time_tag(designation.date), designation.type.value, :right
   end
   
   def link_to_judge(judge, options = {})
