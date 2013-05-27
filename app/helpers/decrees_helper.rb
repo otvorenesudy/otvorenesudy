b1b807e068c02dae46760e9cbc6c513d9375b61b
Samuel Molnar <molnar.samuel@gmail.com>
2013-05-28 01:28:21 +0200

    Add route to DecreePages

--- app/helpers/decrees_helper.rb
+++ app/helpers/decrees_helper.rb
@@ -4,17 +4,17 @@ module DecreesHelper
   def decree_title(decree)
     # TODO for page title
   end
-  
+
   # TODO rm?
   def decree_headline(decree)
     introduction = [decree.legislation_subarea, decree.form].reject(&:blank?).map(&:value)
     natures      = decree.natures.pluck(:value).join(', ')
-    
+
     #["Súdne rozhodnutie", introduction, natures].join(' ').html_safe
-    
+
     [introduction.join(' &ndash; '), natures].join(', ').html_safe
   end
-  
+
   def decree_date(date)
     time_tag date, format: :long 
   end
@@ -35,5 +35,22 @@ module DecreesHelper
     end
 
     external_link_to legislation.value, url, options
+  end
+
+  def decree_to_document_viewer(decree)
+    result = Hash.new
+
+    result[:name]   = decree.file_number
+    result[:number] = 1
+    result[:pages]  = decree.pages.map do |page|
+      { 
+        number:  page.number, 
+        scanUrl: image_decree_page_path(decree, page.number),
+        textUrl: text_decree_page_path(decree, page.number),
+        comments: []
+      }
+    end
+
+    result
   end
 end
