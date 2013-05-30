21200a0d01b94f01f77e813a23f1a01ac73ddcf3
Samuel Molnar <molnar.samuel@gmail.com>
2013-05-30 20:19:48 +0200

    Do add query to decree link

--- app/helpers/decree_pages_helper.rb
+++ app/helpers/decree_pages_helper.rb
@@ -1,6 +1,6 @@
 module DecreePagesHelper
-  def link_to_decree_page(decree, query, page, title, options = {})
-    link = "#{decree_path_with_params(decree, q: query)}#document/1/page/#{page}"
+  def link_to_decree_page(decree, page, title, options = {})
+    link = "#{decree_path_with_params(decree)}#document/1/page/#{page}"
 
     link_to title, link, options
   end
