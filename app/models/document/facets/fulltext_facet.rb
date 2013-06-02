4e6dc439ad69e98e238b982b61868787b3c4bb0f
Samuel Molnar <molnar.samuel@gmail.com>
2013-06-02 22:11:01 +0200

    Add FulltextFacet

---
+++ app/models/document/facets/fulltext_facet.rb
@@ -0,0 +1,28 @@
+module Document::Facets
+  class FulltextFacet < Document::Facets::Facet
+    include Document::Search::Query
+
+    def initialize(name, field, options)
+      super name, field, options.merge!(abstract: true)
+    end
+
+    def build_query
+      build_query_from(@field, @terms, query_options)
+    end
+
+    def build_filter
+      [build_query_filter_from(@field, @terms, query_options)]
+    end
+
+    def parse(value)
+      value.respond_to?(:join) ? value.join(' ').to_s : value.to_s
+    end
+
+    private
+
+    def query_options
+      @query_options ||= { operator: :or, analyze_wildcard: true }
+    end
+
+  end
+end
