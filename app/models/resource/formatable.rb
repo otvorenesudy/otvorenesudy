b5d9b55cb65ed2252156475eca8367edb329caef
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-03 01:19:02 +0200

    Fix a bug in Formattable module

--- app/models/resource/formatable.rb
+++ app/models/resource/formatable.rb
@@ -8,7 +8,7 @@ module Resource::Formatable
     
     def formatable(attribute, options = {})
       define_method attribute do |pattern = nil|
-        return super() if defined?(super) && (pattern.nil? || pattern == options[:default])
+        return super() if defined?(super) == true && (pattern.nil? || pattern == options[:default])
         
         formatter = (self.class.formatters[attribute] ||= Formatter.new data = yield(self), options)
         cache     = (formatted[attribute] ||= Cache.new data || yield(self))
