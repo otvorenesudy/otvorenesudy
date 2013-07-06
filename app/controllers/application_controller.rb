9e3da63d45089c8a0596f36e82af4b86c6de4575
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-07 01:24:46 +0200

    Refactor user views, improve flash messages, configure Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def after_sign_in_path_for(resource_or_scope)
+    # TODO change to subscription managment
+    stored_location_for(resource_or_scope) || edit_user_registration_path
+  end
+      
+  def after_sign_out_path_for(resource_or_scope)
+    new_session_path(resource_name)
+  end
 end
