5452111710f47e619d509883e1daf119a9f49353
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-08 23:11:05 +0200

    Fix several bugs in Devise, update mailer templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  def after_sign_in_path_for(resource_or_scope)
+  protected
+  
+  def after_sign_in_path_for(resource)
     # TODO change to subscription managment
-    stored_location_for(resource_or_scope) || edit_user_registration_path
+    stored_location_for(resource) || edit_user_registration_path
   end
       
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(resource)
     new_session_path(resource_name)
+  end
+  
+  def after_update_path_for(resource)
+    edit_user_registration_path
   end
 end
