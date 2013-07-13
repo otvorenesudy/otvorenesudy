105719aba291d6340f0f2262e4b3e8547f4963b7
Samuel Molnar <molnar.samuel@gmail.com>
2013-07-13 19:28:58 +0200

    Refactor ActionMailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ OpenCourts::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'otvorenesudy.sk' }
 
   # Enable threaded mode
   # config.threadsafe!
@@ -64,6 +65,4 @@ OpenCourts::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  #
-  config.action_mailer.default_url_options = { :host => 'otvorenesudy.sk' }
 end
