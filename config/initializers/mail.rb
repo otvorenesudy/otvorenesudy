f7e3d949f8a2494600e7af5332b8e97b0cd57d07
Samuel Molnar <molnar.samuel@gmail.com>
2013-07-13 18:19:15 +0200

    Perform deliveries everywhere except test env

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,12 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "otvorenesudy.sk",
-  :user_name            => Configuration.mailer.username,
-  :password             => Configuration.mailer.password,
-  :authentication       => 'plain',
-  :enable_starttls_auto => true
-}
-
+unless Rails.env.test?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "otvorenesudy.sk",
+    :user_name            => Configuration.mailer.username,
+    :password             => Configuration.mailer.password,
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+end
