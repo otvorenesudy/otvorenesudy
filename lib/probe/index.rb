9772b8f9bf2f9f10549eca08f403a9b61badba18
Samuel Molnar <molnar.samuel@gmail.com>
2013-07-13 03:02:26 +0200

    Format code

--- lib/probe/index.rb
+++ lib/probe/index.rb
@@ -10,7 +10,7 @@ module Probe
       def setup
         settings
 
-        after_save :update_index
+        after_save    :update_index
         after_destroy :update_index
 
         index_name "#{index_name}_#{Rails.env}"
