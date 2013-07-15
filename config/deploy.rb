8229053150d5decde7bb4ce1be96b30f49b6f19d
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-16 01:07:43 +0200

    Format deployment configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,61 +1,61 @@
 require 'bundler/capistrano'
 require 'resque'
 require 'rvm/capistrano'
-require "capistrano-resque"
-#require "whenever/capistrano"
+require 'capistrano-resque'
+#require 'whenever/capistrano'
 
-set :stages,        [:staging, :production]
+set :stages, [:staging, :production]
+
 require 'capistrano/ext/multistage'
 
-set :application,    "opencourts"
+set :application,    'opencourts'
 set :scm,            :git
-set :repository,     "git@github.com:otvorenesudy/otvorenesudy-dev.git"
-set :scm_passphrase, ""
-set :user,           "deploy"
+set :repository,     'git@github.com:otvorenesudy/otvorenesudy-dev.git'
+set :scm_passphrase, ''
+set :user,           'deploy'
 
 set :use_sudo, false
 
-set :ssh_options,           { :forward_agent => true }
-set :deploy_via,            :remote_cache
+set :ssh_options, { forward_agent: true }
+set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 
 # Whenever
 #set :whenever_command, "RAILS_ENV=#{rails_env} bundle exec whenever"
 
 # Resque
-set :workers, { :probe => 4 }
+set :workers, { probe: 4 }
 
 default_run_options[:pty] = true
 
 namespace :db do
-
-  desc "Migrate Production Database"
+  desc "Migrate DB"
   task :create, roles: :db do
     run "cd #{current_path}; rake db:create RAILS_ENV=#{rails_env}"
   end
 
-  desc "Migrate Production Database during release"
+  desc "Migrate DB during release"
   task :create_release, roles: :db do
     run "cd #{release_path}; rake db:create RAILS_ENV=#{rails_env}"
   end
 
-  desc "Setup db during deployment of release for this environment"
-  task :setup_release, :roles => :db do
+  desc "Setup DB during deployment of release for this environment"
+  task :setup_release, roles: :db do
     run "cd #{release_path}; RAILS_ENV=#{rails_env} rake db:setup"
   end
 
-  desc "Setup current db for this environment"
-  task :setup_current, :roles => :db do
+  desc "Setup current DB for this environment"
+  task :setup_current, roles: :db do
     run "cd #{current_path}; RAILS_ENV=#{rails_env} rake db:setup"
   end
 
-  desc "Drop database for this environment"
+  desc "Drop DB for this environment"
   task :drop, roles: :db do
     run "cd #{current_path}; RAILS_ENV=#{rails_env} rake db:drop"
   end
 
-  desc "Reset database for this environment"
-  task :reset, :roles => :db do
+  desc "Reset DB for this environment"
+  task :reset, roles: :db do
     run "cd #{current_path}; RAILS_ENV=#{rails_env} rake db:reset"
   end
 end
@@ -88,10 +88,14 @@ end
 
 # If you are using Passenger mod_rails uncomment this
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  task :start do
+  end
+  
+  task :stop do
+  end
+  
+  task :restart, roles: :app, except: { no_release: true } do
+    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
   end
 
   desc "Symlink shared"
