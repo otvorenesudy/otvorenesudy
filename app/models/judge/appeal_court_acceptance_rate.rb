bee0b1b1dd66ea7611eabe28f1e38d33a3812193
Samuel Molnar <molnar.samuel@gmail.com>
2013-07-15 23:10:47 +0200

    Add Judge indicators

--- app/models/judge/appeal_court_acceptance_rate.rb
+++ app/models/judge/appeal_court_acceptance_rate.rb
@@ -1,30 +1,24 @@
 module Judge::AppealCourtAcceptanceRate
   def appeal_court_acceptance_rate
-    @appeal_court_acceptance_rate ||= compute_appeal_court_acceptance_rate
-  end
-
-  alias :appeal_court_acceptance_rate? :appeal_court_acceptance_rate
-
-  private
-
-  def compute_appeal_court_acceptance_rate
-    tables = statistical_tables.by_name("O")
+    type      = statistical_summaries.by_year.first.court.type
+    summaries = statistical_summaries.by_court_type(type)
 
     accepted = 0
     all      = 0
 
-    return unless tables.any?
+    return unless summaries.any?
 
-    # TODO: equation (accepted_2011 + accepted_2012)/(all_2011 + all_2012)
-    tables.each do |table|
-      table.rows.each do |row|
-        sum = row.cells.pluck(:value).map(&:to_i).sum
+    summaries.each do |summary|
+      summary.tables.by_name('O').each do |table|
+        table.rows.each do |row|
+          sum = row.cells.pluck(:value).map(&:to_i).sum
 
-        if row.name.value == "sv_Pocet1"
-          accepted += sum
+          if row.name.value == "sv_Pocet1"
+            accepted += sum
+          end
+
+          all += sum
         end
-
-        all += sum
       end
     end
 
