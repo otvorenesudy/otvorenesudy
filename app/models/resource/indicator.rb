4bf9f2136399457b8e98421d1f6c223e1fb3ffda
Samuel Molnar <molnar.samuel@gmail.com>
2013-07-15 23:11:19 +0200

    Implement simple cache for indicators on instance level

---
+++ app/models/resource/indicator.rb
@@ -0,0 +1,36 @@
+module Resource::Indicator
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def indicators
+      @indicators ||= Hash.new
+    end
+
+    def invalidate_indicators
+      indicators.each do |_, values|
+        values.clear
+      end
+    end
+
+    private
+
+    def register_indicator(indicator, options = {})
+      name      = options[:method] || indicator.name.split(/::/).last.underscore.to_sym
+      attribute = options[:attribute] || :id
+
+      indicators[name] ||= Hash.new
+
+      self.class_eval do
+        include indicator
+
+        alias_method "memoized_#{name}".to_sym, name.to_sym
+
+        define_method name.to_sym do
+          self.class.indicators[name][self.read_attribute(attribute)] ||= send("memoized_#{name}".to_sym)
+        end
+
+        alias_method "#{name}?".to_sym, name.to_sym
+      end
+    end
+  end
+end
