ea0d84e3c82ffd3683cd28845dd2424776bfeb8a
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-16 20:20:48 +0200

    Format some code

--- app/models/judge_statistical_summary.rb
+++ app/models/judge_statistical_summary.rb
@@ -14,9 +14,25 @@ class JudgeStatisticalSummary < ActiveRecord::Base
                   :substantiation_notes,
                   :court_chair_actions
 
+  scope :by_prominent_court_type, lambda {
+    types = joins(:court).group('courts.court_type_id').order(:count_all).count
+
+    return unless types.any?
+
+    if types.values.uniq.size == 1
+      summaries = where(year: self.order(:year).last.year)
+      max       = summaries.map(&:assigned_issues_count).max
+
+      type = summaries.find { |s| s.assigned_issues_count == max }.court_type
+    else
+      type = CourtType.find(types.keys.last)
+    end
+
+    by_court_type(type)
+  }
+
   scope :by_court_type, lambda { |type| joins(:court).where(:'courts.court_type_id' => type.id) }
   scope :by_year, lambda { order(:year) }
-
 
   belongs_to :court
   delegate :type, to: :court, prefix: true # TODO: creates delegation to court.type by court_type
@@ -31,24 +47,9 @@ class JudgeStatisticalSummary < ActiveRecord::Base
   # TODO: validate presence of date and author
   validates :year,   presence: true
 
-  scope :by_prominent_court_type, lambda {
-    types = joins(:court).group('courts.court_type_id').order(:count_all).count
+  private
 
-    return unless types.any?
-
-    if types.values.uniq.size == 1
-      summaries = where(year: self.order(:year).last.year)
-      max       = summaries.map(&:assign_issues_count).max
-
-      type = summaries.find { |s| s.assign_issues_count == max }.court_type
-    else
-      type = CourtType.find(types.keys.last)
-    end
-
-    by_court_type(type)
-  }
-
-  def assign_issues_count
+  def assigned_issues_count
     tables.by_name('P').map { |table| table.rows[0].cells.pluck(:value).map(&:to_i).sum }.sum
   end
 end
