7579615ab24f1b221a0ab1998deba379ea672b86
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-16 23:57:01 +0200

    Simplify analytics helper

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -2,8 +2,4 @@ module AnalyticsHelper
   def track_data(value)
     { :'data-track' => value }
   end
-
-  def track_data_attr(value)
-    track_data(value).map { |k, v| "#{k}=\"#{v}\"" }.join(' ').html_safe
-  end
 end
