17f252d7316e3390953134c7cc2d3e386b197d81
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-18 21:52:00 +0200

    Add new Markdown engine draft

--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -5,3 +5,16 @@ MarkdownRails.configure do |config|
     markdown.render(markdown_source)
   end
 end
+
+# TODO rm?
+#class ActionView::Template::Handlers::Markdown
+#  class_attribute :default_format
+#  self.default_format = Mime::HTML
+#  def call(template)
+#    @erb ||= ActionView::Template.registered_template_handler :erb
+#    @md  ||= Redcarpet::Markdown.new Redcarpet::Render::HTML, autolink: true, fenced_code_blocks: true
+#    @md.render(template.source).html_safe
+#  end     
+#end
+# 
+#ActionView::Template.register_template_handler :md, ActionView::Template::Handlers::Markdown.new
