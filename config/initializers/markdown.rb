fb9d99aa3ce03b55827ce1318d01fb8aee2068be
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-21 18:28:44 +0200

    Update Markdown handler

--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,20 +1,8 @@
-MarkdownRails.configure do |config|
-  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true, autolink: true)
-
-  config.render do |markdown_source|
-    markdown.render(markdown_source)
-  end
+class ActionView::Template::Handlers::Markdown
+  def call(template)
+    compiled = (@erb ||= ActionView::Template.registered_template_handler :erb).call(template)
+    "Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, fenced_code_blocks: true).render(begin;#{compiled};end)"
+  end     
 end
-
-# TODO rm?
-#class ActionView::Template::Handlers::Markdown
-#  class_attribute :default_format
-#  self.default_format = Mime::HTML
-#  def call(template)
-#    @erb ||= ActionView::Template.registered_template_handler :erb
-#    @md  ||= Redcarpet::Markdown.new Redcarpet::Render::HTML, autolink: true, fenced_code_blocks: true
-#    @md.render(template.source).html_safe
-#  end     
-#end
-# 
-#ActionView::Template.register_template_handler :md, ActionView::Template::Handlers::Markdown.new
+ 
+ActionView::Template.register_template_handler :md, ActionView::Template::Handlers::Markdown.new
