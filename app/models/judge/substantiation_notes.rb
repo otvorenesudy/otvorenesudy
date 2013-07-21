ed57ddd2ffe3e58856c15b325306690b21715f6a
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-21 15:55:02 +0200

    Hide blank substantiation notes from judge profile

--- app/models/judge/substantiation_notes.rb
+++ app/models/judge/substantiation_notes.rb
@@ -2,8 +2,8 @@ module Judge::SubstantiationNotes
   extend ActiveSupport::Concern
   
   def substantiation_notes
-    @substantiation_notes ||= statistical_summaries.order('year desc').map do |summary|
-      [summary.year, summary.substantiation_notes]
-    end
+    @substantiation_notes ||= statistical_summaries.order('year desc').map { |summary|
+      [summary.year, summary.substantiation_notes] unless summary.substantiation_notes.blank? 
+    }.reject(&:nil?)
   end
 end
