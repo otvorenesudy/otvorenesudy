d14a4d58453125d5baed3d00faa9a34a46472261
Samuel Molnar <molnar.samuel@gmail.com>
2013-07-25 19:35:16 +0200

    Hide secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-OpenCourts::Application.config.secret_token = '237b223ead305d585993e6a806fdce2070b032c653209ccb872021405127b8c379c76bcdcd56bd7f3cd56c5564e347e9a17e858c197bf5239edc2e4ceb0ae179'
+
+OpenCourts::Application.config.secret_token = ::Configuration.secret_token
