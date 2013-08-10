52c7b40816e8f4a78d63fed45fd08d03e531f931
Samuel Molnar <molnar.samuel@gmail.com>
2013-08-10 16:12:41 +0200

    Remove Mapper from Alias

--- lib/probe/index/alias.rb
+++ lib/probe/index/alias.rb
@@ -1,26 +1,24 @@
 class Probe::Index
   module Alias
-    module Mapper
-      # TODO: use when elasticsearch support percolating against index alias
+    # TODO: use when elasticsearch support percolating against index alias
 
-      def index_alias(name = nil)
-        Tire::Alias.new(name: name || index.name)
-      end
+    def index_alias(name = nil)
+      Tire::Alias.new(name: name || index.name)
+    end
 
-      def alias_as(bulk_index)
-        delete
+    def alias_as(bulk_index)
+      delete
 
-        index = index_alias
+      index = index_alias
 
-        index.indices.clear
-        index.index(bulk_index.name)
+      index.indices.clear
+      index.index(bulk_index.name)
 
-        index.save
-      end
+      index.save
+    end
 
-      def generate_alias_name
-        "#{index_name}_#{Time.now.strftime("%Y%m%d%H%M")}"
-      end
+    def generate_alias_name
+      "#{index_name}_#{Time.now.strftime("%Y%m%d%H%M")}"
     end
   end
 end
