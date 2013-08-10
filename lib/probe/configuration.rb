97f9172e8531b91a2d4e773d34251dbca9408950
Samuel Molnar <molnar.samuel@gmail.com>
2013-08-10 16:12:11 +0200

    Refactor indices definition

--- lib/probe/configuration.rb
+++ lib/probe/configuration.rb
@@ -1,11 +1,11 @@
 module Probe
   class Configuration < Settingslogic
-    source File.join(Rails.root, 'config', 'probe.yml')
+    source Rails.root.join('config', 'probe.yml')
 
     namespace Rails.env
 
     def self.models
-      @models ||= indices.map { |e| e.singularize.to_sym }
+      @models ||= indices.map { |e| e.constantize }
     end
   end
 end
