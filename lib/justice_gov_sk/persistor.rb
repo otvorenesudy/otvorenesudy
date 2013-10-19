a33b233a03493088176c1b600a5cb49f55bd4dee
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-19 13:53:53 +0200

    Apply temporary hotfix on Hearing indexing in ES

--- lib/justice_gov_sk/persistor.rb
+++ lib/justice_gov_sk/persistor.rb
@@ -2,19 +2,38 @@ module JusticeGovSk
   class Persistor
     include Core::Persistor
 
-    # TODO rm
-    #def persist(instance)
-    #  super
-    #
-    #  if instance.respond_to? :update_index
-    #    print "Updating index #{identify instance} ... "
-    #
-    #    instance.update_index
-    #
-    #    puts "done (#{identify instance})"
-    #  end
-    #
-    #  instance
-    #end
+    def persist(instance)
+      super
+
+      # TODO refactor this fix
+      Hearing.find(instance.id).save! if instance.is_a? Hearing
+
+      # TODO rm debug
+      #if instance.is_a? Hearing
+      #  a = instance
+      #  b = Hearing.find(instance.id)
+      #
+      #  puts "EQL #{a == b}"
+      #  puts "CLS #{a.class} #{b.class}"
+      #  puts a.inspect
+      #  puts b.inspect
+      #
+      #  a.save!
+      #  #b.save!
+      #end
+
+      # TODO rm
+      #super
+      #
+      #if instance.respond_to? :update_index
+      #  print "Updating index #{identify instance} ... "
+      #
+      #  instance.update_index
+      #
+      #  puts "done (#{identify instance})"
+      #end
+      #
+      #instance
+    end
   end
 end
