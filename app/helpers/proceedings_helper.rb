49343c2094dae1ae81c1ae11735b35611bbbbbfd
Pavol Zbell <pavol.zbell@gmail.com>
2013-12-29 23:40:20 +0100

    Add view helpers for proceedings

--- app/helpers/proceedings_helper.rb
+++ app/helpers/proceedings_helper.rb
@@ -1,9 +1,25 @@
+# encoding: utf-8
+
 module ProceedingsHelper
+  def proceeding_title(proceeding)
+    title(*proceeding_identifiers(proceeding) << 'Súdne konanie')
+  end
+
+  def proceeding_headline(proceeding, options = {})
+    join_and_truncate proceeding_identifiers(proceeding), { separator: ' &ndash; ' }.merge(options)
+  end
+
   def proceeding_date(date, options = {})
     time_tag date.to_date, { format: :long }.merge(options)
   end
-  
+
   def link_to_proceeding(proceeding, body, options = {})
     link_to body, proceeding_path(proceeding), options
+  end
+
+  private
+
+  def proceeding_identifiers(proceeding)
+    ["IČS #{proceeding.file_number}"]
   end
 end
