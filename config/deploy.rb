cd19245d77547c8c16659cab1295ed8967b8515a
Samuel Molnar <molnar.samuel@gmail.com>
2014-03-03 23:30:09 +0100

    Use Bundler everywhere

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,32 +27,32 @@ default_run_options[:pty] = true
 namespace :db do
   desc "Migrate DB"
   task :create, roles: :db do
-    run "cd #{current_path}; rake db:create RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; bundle exec rake db:create RAILS_ENV=#{rails_env}"
   end
 
   desc "Migrate DB during release"
   task :create_release, roles: :db do
-    run "cd #{release_path}; rake db:create RAILS_ENV=#{rails_env}"
+    run "cd #{release_path}; bundle exec rake db:create RAILS_ENV=#{rails_env}"
   end
 
   desc "Setup DB during deployment of release for this environment"
   task :setup_release, roles: :db do
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} rake db:setup"
+    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake db:setup"
   end
 
   desc "Setup current DB for this environment"
   task :setup_current, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} rake db:setup"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake db:setup"
   end
 
   desc "Drop DB for this environment"
   task :drop, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} rake db:drop"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake db:drop"
   end
 
   desc "Reset DB for this environment"
   task :reset, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} rake db:reset"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake db:reset"
   end
 end
 
@@ -60,27 +60,27 @@ end
 namespace :probe do
   desc "Drop Probe indices"
   task :drop do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} rake probe:drop"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:drop"
   end
 
   desc "Import Probe indices"
   task :import, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} rake probe:import"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:import"
   end
 
   desc "Enqueue async import of Probe indices"
   task :import_async, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} rake probe:import:async"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:import:async"
   end
 
   desc "Update Probe indices"
   task :update, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} rake probe:update"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:update"
   end
 
   desc "Enqueue async update of Probe indices"
   task :update_async, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} rake probe:update:async"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:update:async"
   end
 end
 
@@ -127,6 +127,6 @@ namespace :deploy do
   after 'deploy:update_code', 'deploy:symlink_shared', 'deploy:move_in_configuration_files', 'db:create_release', 'deploy:migrate'
 
   after 'deploy:update_code' do
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} rake assets:precompile"
+    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
   end
 end
