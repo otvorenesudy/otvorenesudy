cffe00f3ea456556fad5e7c72f4f17befd62e6da
Pavol Zbell <pavol.zbell@gmail.com>
2014-05-21 11:55:14 +0200

    Update ApplicationHelper and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,18 +5,26 @@ module ApplicationHelper
     'Otvorené súdy'
   end
 
-  def title(*values)
-    (values << default_title).map { |value| html_escape value }.join(' &middot; ').html_safe
-  end
-
-  def obtain(value)
+  def resolve_title(value)
     return default_title if value.blank?
     return title(value) unless value.end_with? default_title
 
     value
   end
 
+  def title(*values)
+    (values << default_title).map { |value| html_escape value }.join(' &middot; ').html_safe
+  end
+
   def canonical_url
     "http://#{request.host}#{request.fullpath}"
+  end
+
+  def url_to_organization(path = nil)
+    File.join('https://github.com', Configuration.github.organization, path.to_s).sub(/\/\z/, '')
+  end
+
+  def url_to_repository(path = nil)
+    url_to_organization "#{Configuration.github.repository}/#{path}"
   end
 end
