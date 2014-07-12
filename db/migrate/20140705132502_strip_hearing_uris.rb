7042c973fb89c60175b2afb0990ae315ea211bbc
Samuel Molnar <molnar.samuel@gmail.com>
2014-07-12 17:58:11 +0200

    Skip migration

--- db/migrate/20140705132502_strip_hearing_uris.rb
+++ db/migrate/20140705132502_strip_hearing_uris.rb
@@ -1,18 +1,18 @@
 class StripHearingUris < ActiveRecord::Migration
   def up
-    models = [Judge, Hearing]
+    #models = [Judge, Hearing]
 
-    models.each do |model|
-      model.where("uri LIKE '% %'").find_each do |record|
-        record.uri_will_change!
-        record.uri.strip!
+    #models.each do |model|
+      #model.where("uri LIKE '% %'").find_each do |record|
+        #record.uri_will_change!
+        #record.uri.strip!
 
-        # TODO resolve non-unique urls after strip
-        record.save! unless model.exists?(uri: record.uri)
-      end
-    end
+        ## TODO resolve non-unique urls after strip
+        #record.save! unless model.exists?(uri: record.uri)
+      #end
+    #end
+  end
 
-    def down
-    end
+  def down
   end
 end
