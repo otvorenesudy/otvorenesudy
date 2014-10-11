23aa4ec0ecc0021a8a473ced388bdb01cb240a85
Pavol Zbell <pavol.zbell@gmail.com>
2014-10-11 22:44:13 +0200

    Add selection procedure view

--- app/controllers/selection_procedures_controller.rb
+++ app/controllers/selection_procedures_controller.rb
@@ -2,6 +2,9 @@ class SelectionProceduresController < SearchController
   def show
     @procedure = SelectionProcedure.find(params[:id])
 
+    @candidates    = @procedure.candidates.order(:rank, :name)
+    @commissioners = @procedure.commissioners.order(:name)
+
     # TODO
     # * handle storage documents for selection procedure
     # * handle storage documents for selection procedure candidate in SelectionProcedureCandidateController
