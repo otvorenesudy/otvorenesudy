268fca4408a1d4c84020ebaa15bb6538012d0f48
Pavol Zbell <pavol.zbell@gmail.com>
2014-10-11 19:19:51 +0200

    Add person counter facets to selection procedures

--- app/models/selection_procedure.rb
+++ app/models/selection_procedure.rb
@@ -25,14 +25,16 @@ class SelectionProcedure < ActiveRecord::Base
     map :id
 
     analyze :organization_name
-    analyze :date,          type: :date
-    analyze :closed_at,     type: :date
+    analyze :date,                type: :date
+    analyze :closed_at,           type: :date
     analyze :place
     analyze :position
     analyze :state
     analyze :workplace
-    analyze :candidates,    as: lambda { |p| p.candidates.pluck(:name) }
-    analyze :commissioners, as: lambda { |p| p.commissioners.pluck(:name) }
+    analyze :candidates,                          as: lambda { |p| p.candidates.pluck(:name) }
+    analyze :candidates_count,    type: :integer, as: lambda { |p| p.candidates.count }
+    analyze :commissioners,                       as: lambda { |p| p.commissioners.pluck(:name) }
+    analyze :commissioners_count, type: :integer, as: lambda { |p| p.commissioners.count }
 
     sort_by :date, :closed_at, :created_at
   end
