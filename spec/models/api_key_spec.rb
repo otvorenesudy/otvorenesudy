d1eadbe1f79ee3fa0d26f30ffb52101b1e35aed9
Pavol Zbell <pavol.zbell@gmail.com>
2014-12-01 15:53:07 +0100

    Rename ApiKey.key to value

--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -4,24 +4,24 @@ describe ApiKey do
   describe 'callbacks' do
     describe '#generate_key' do
       it 'generates key' do
-        allow(ApiKey::Generator).to receive(:generate).and_return('key')
+        allow(ApiKey::Generator).to receive(:generate).and_return('value')
 
         api_key = create :api_key
 
-        expect(api_key.key).to eql('key')
+        expect(api_key.value).to eql('value')
       end
 
       context 'when key already exists' do
         it 'tries to generate another one in a loop' do
-          allow(ApiKey::Generator).to receive(:generate).and_return('key')
+          allow(ApiKey::Generator).to receive(:generate).and_return('value')
 
           create :api_key
 
-          allow(ApiKey::Generator).to receive(:generate).and_return('key', 'another key')
+          allow(ApiKey::Generator).to receive(:generate).and_return('value', 'another value')
 
           api_key = create :api_key
 
-          expect(api_key.key).to eql('another key')
+          expect(api_key.value).to eql('another value')
         end
       end
     end
