284dee87d8fb47277708f739d4b2d66895fa93fb
Samuel Molnar <molnar.samuel@gmail.com>
2014-12-13 10:58:01 +0100

    Add chart for judge

--- app/models/judge/indicators.rb
+++ app/models/judge/indicators.rb
@@ -14,15 +14,52 @@ class Judge
 
     def self.load!
       @data = Hash.new
+      @numerical_average = nil
 
-      CSV.read(Rails.root.join('data', 'judge_indicators.csv'), col_sep: "\t", headers: true).each do |line|
+      CSV.read(Rails.root.join('data', 'judges_statistical_indicators.csv'), col_sep: "\t", headers: true).each do |line|
         judge = Judge.find_by_name(line[0])
 
-        @data[judge.id] = normalize_values(line) if judge
+        next unless judge
+
+        @data[judge.id] ||= OpenStruct.new
+        @data[judge.id].statistical = normalize_statistical_values(line)
+      end
+
+      CSV.read(Rails.root.join('data', 'judges_numerical_indicators.csv'), col_sep: "\t", headers: true).each do |line|
+        judge = Judge.find_by_name(line[0])
+
+        next unless judge
+
+        @data[judge.id] ||= OpenStruct.new
+        @data[judge.id].numerical = normalize_numerical_values(line)
       end
     end
 
-    def self.normalize_values(values)
+    def self.numerical_average
+      load! unless @data
+
+      @numerical_average ||= calculate_numerical_average
+    end
+
+    def self.calculate_numerical_average
+      average = Hash.new(0)
+
+      @data.each do |id, values|
+        next unless values.numerical
+
+        values.numerical.each do |key, value|
+          average[key] += value if value
+        end
+      end
+
+      average.each do |key, value|
+        average[key] = value / @data.size.to_f
+      end
+
+      average
+    end
+
+    def self.normalize_statistical_values(values)
       values.each_with_index do |(key, value), index|
         value.gsub!(/-/, '–')
 
@@ -37,6 +74,16 @@ class Judge
                   else value
                   end
         end
+
+        values[key] = value
+      end
+    end
+
+    def self.normalize_numerical_values(values)
+      values.each_with_index do |(key, value), index|
+        value = case index
+                when 2..9 then value.to_i
+                end
 
         values[key] = value
       end
