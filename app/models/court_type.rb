efe98c007c24f99f0a0d370248ab39f1e17e7bab
Milos Blasko <blaskomm@gmail.com>
2015-07-28 23:12:47 +0200

    Refactored court_type translations

--- app/models/court_type.rb
+++ app/models/court_type.rb
@@ -9,9 +9,9 @@ class CourtType < ActiveRecord::Base
 
   validates :value, presence: true
 
-  value :constitutional, I18n.t('.constitutional')
-  value :supreme,        I18n.t('.supreme')
-  value :specialized,    I18n.t('.specialized')
-  value :regional,       I18n.t('.regional')
-  value :district,       I18n.t('.district')
+  value :constitutional, I18n.t('court_type.constitutional')
+  value :supreme,        I18n.t('court_type.supreme')
+  value :specialized,    I18n.t('court_type.specialized')
+  value :regional,       I18n.t('court_type.regional')
+  value :district,       I18n.t('court_type.district')
 end
