3747f2a7324e2e5f27212a94e568b2083ac3a9ab
Samuel Molnar <molnar.samuel@gmail.com>
2015-10-08 18:04:31 +0200

    Update deploy.rb to Capistrano 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,131 +1,86 @@
-require 'bundler/capistrano'
-require 'sidekiq'
-require 'rvm/capistrano'
-require 'capistrano/sidekiq'
+# config valid only for current version of Capistrano
+lock '3.4.0'
 
-set :stages, [:staging, :production]
+set :user, 'deploy'
 
-require 'capistrano/ext/multistage'
+# Repository
+set :application, 'opencourts'
+set :scm,         :git
+set :repo_url,    'git@github.com:otvorenesudy/otvorenesudy.git'
 
-set :application,    'opencourts'
-set :scm,            :git
-set :repository,     'git@github.com:otvorenesudy/otvorenesudy.git'
-set :scm_passphrase, ''
-set :user,           'deploy'
+# RVM
+set :rvm_type,         :user
+set :rvm_ruby_version, '2.2.2'
 
-set :use_sudo, false
+# Links
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/configuration.yml')
+set :linked_dirs,  fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'storage')
 
-set :ssh_options, { forward_agent: true }
-set :deploy_via, :remote_cache
-set :git_enable_submodules, 1
+# Whenever
+set :whenever_identifier, ->{ "#{fetch(:application)}-#{fetch(:stage)}" }
 
 set :keep_releases, 1
-
-default_run_options[:pty] = true
-
-# Database (PostgreSQL)
-namespace :db do
-  desc "Migrate DB"
-  task :create, roles: :db do
-    run "cd #{current_path}; bundle exec rake db:create RAILS_ENV=#{rails_env}"
-  end
-
-  desc "Migrate DB during release"
-  task :create_release, roles: :db do
-    run "cd #{release_path}; bundle exec rake db:create RAILS_ENV=#{rails_env}"
-  end
-
-  desc "Setup DB during deployment of release for this environment"
-  task :setup_release, roles: :db do
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake db:setup"
-  end
-
-  desc "Setup current DB for this environment"
-  task :setup_current, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake db:setup"
-  end
-
-  desc "Drop DB for this environment"
-  task :drop, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake db:drop"
-  end
-
-  desc "Reset DB for this environment"
-  task :reset, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake db:reset"
-  end
-end
-
-# Probe (Elasticsearch)
-namespace :probe do
-  desc "Drop Probe indices"
-  task :drop do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:drop"
-  end
-
-  desc "Import Probe indices"
-  task :import, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:import"
-  end
-
-  desc "Enqueue async import of Probe indices"
-  task :import_async, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:import:async"
-  end
-
-  desc "Update Probe indices"
-  task :update, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:update"
-  end
-
-  desc "Enqueue async update of Probe indices"
-  task :update_async, roles: :db do
-    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec rake probe:update:async"
-  end
-end
+set :deploy_via, :remote_cache
+set :git_enable_submodules, 1
+set :ssh_options, {
+  forward_agent: true
+}
+set :use_sudo, false
 
 namespace :workers do
-  desc "Start workers"
+  desc 'Start workers'
   task :start do
-    run 'sidekiq -C config/sidekiq.yml'
-  end
-
-  desc "Stop workers"
-  task :stop do
-    run 'sidekiqctl stop tmp/pids/sidekiq.pid 30'
-  end
-end
-
-# General
-namespace :deploy do
-  [:start, :stop, :upgrade].each do |command|
-    desc "#{command.to_s.capitalize} unicorn server"
-    task command, roles: :app, except: { no_release: true } do
-      run "/etc/init.d/unicorn-#{application}-#{rails_env} #{command}"
+    on roles(:app) do
+      within current_path do
+        run 'sidekiq -C config/sidekiq.yml'
+      end
     end
   end
 
-  desc 'Restart Unicorn'
-  task :restart do
-    run "/etc/init.d/unicorn-#{application}-#{rails_env} stop"
-    run "/etc/init.d/unicorn-#{application}-#{rails_env} start"
-  end
-
-  desc "Symlink shared"
-  task :symlink_shared, roles: :app do
-    run "ln -nfs #{shared_path} #{release_path}/shared"
-    run "ln -nfs #{shared_path}/storage #{release_path}"
-  end
-
-  desc "Move in configuration files"
-  task :move_in_configuration_files, roles: :app do
-    run "ln -nfs #{shared_path}/config/*.yml #{release_path}/config"
-  end
-
-  after 'deploy', 'deploy:cleanup'
-  after 'deploy:update_code', 'deploy:symlink_shared', 'deploy:move_in_configuration_files', 'db:create_release', 'deploy:migrate'
-
-  after 'deploy:update_code' do
-    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
+  desc 'Stop workers'
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        run 'sidekiqctl stop tmp/pids/sidekiq.pid 30'
+      end
+    end
   end
 end
+
+namespace :deploy do
+  after 'deploy:publishing', 'deploy:restart'
+  after 'finishing', 'deploy:cleanup'
+
+  desc 'Deploy app for first time'
+  task :cold do
+    invoke 'deploy:starting'
+    invoke 'deploy:started'
+    invoke 'deploy:updating'
+    invoke 'bundler:install'
+    invoke 'deploy:setup_database' # This replaces deploy:migrations
+    invoke 'deploy:compile_assets'
+    invoke 'deploy:normalize_assets'
+    invoke 'deploy:publishing'
+    invoke 'deploy:published'
+    invoke 'deploy:finishing'
+    invoke 'deploy:finished'
+  end
+
+  desc 'Setup database'
+  task :setup_database do
+    on roles(:db) do
+      within release_path do
+        with rails_env: (fetch(:rails_env) || fetch(:stage)) do
+          execute :rake, 'db:create:all'
+          execute :rake, 'db:migrate'
+        end
+      end
+    end
+  end
+
+  task :restart do
+    invoke 'unicorn:legacy_restart'
+  end
+end
+
+# TODO: Probe
