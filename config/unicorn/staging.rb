e8eb72cb70f9b4e5ac617b43643ce6735248aaf8
Samuel Molnar <molnar.samuel@gmail.com>
2015-10-09 02:42:52 +0200

    Do not preload app

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -2,7 +2,7 @@ root = File.expand_path "#{File.dirname(__FILE__)}/../.."
 
 worker_processes 1
 timeout          60
-preload_app      true
+preload_app      false
 listen           "#{root}/tmp/.unicorn.sock", backlog: 64
 pid              "#{root}/tmp/pids/unicorn.pid"
 stderr_path      "#{root}/log/unicorn.log"
