4648afa4e3c21a3c20e9d2e9d9f2b53f61732550
Samuel Molnar <molnar.samuel@gmail.com>
2016-01-24 18:17:20 +0100

    Fix synchronization logic one more

--- app/services/repository_manager.rb
+++ app/services/repository_manager.rb
@@ -8,31 +8,30 @@ class RepositoryManager
   end
 
   def synchronize
-    batch = relation.select('id, updated_at')
+    batch = relation.to_a
 
     results = client.search(
       index: model.index_name,
       type: model.document_type,
       body: {
         filter: {
-          not: {
-            or: batch.map { |record|
-              {
-                and: [
-                  { term: { id: record.id } },
-                  { term: { :'updated_at.untouched' => record.updated_at } }
-                ]
-              }
-            },
-          }
+          or: batch.map { |record|
+            {
+              and: [
+                { term: { id: record.id } },
+                { term: { :'updated_at.untouched' => record.updated_at } }
+              ]
+            }
+          },
         },
         size: batch.size,
         fields: [:id]
       }
     )
 
-    unsynchronized = results['hits']['hits'].map { |e| e['fields']['id'][0].to_i }
+    synchronized = results['hits']['hits'].map { |e| e['fields']['id'][0].to_i }
+    unsynchronized = batch.select { |record| !record.id.in?(synchronized) }
 
-    model.where(id: unsynchronized).find_each(&:update_index)
+    unsynchronized.each(&:update_index)
   end
 end
