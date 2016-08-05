28381b6c9912d5f9f5a0cf9c100069a19a7e912b
Pavol Zbell <pavol.zbell@gmail.com>
2016-08-05 16:02:23 +0200

    Remove some TODOs

--- app/helpers/translation_helper.rb
+++ app/helpers/translation_helper.rb
@@ -1,20 +1,17 @@
 module TranslationHelper
+  def missing_translation?(key)
+    translate(key, default: '__missing__') == '__missing__'
+  end
+
   def locale_specific_spaces(s)
     I18n.locale == :sk ? s.gsub(/\s([aikosuvz])\s/i, ' \1&nbsp;') : s
   end
 
-  # TODO rm??
   def translate_with_count(count, key, options = {})
     translate "counts.#{key}", count: count < 5 ? count : number_with_delimiter(count, options)
   end
 
-  # TODO rm??
   def translate_without_count(count, key)
     translate key, count: count
-  end
-
-  # TODO rm??
-  def missing_translation?(key)
-    translate(key, default: '__missing__') == '__missing__'
   end
 end
