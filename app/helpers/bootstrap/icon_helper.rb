986a01cd62c0d4523e64eae8bc007823bc419d29
Pavol Zbell <pavol.zbell@gmail.com>
2016-08-06 01:28:13 +0200

    Finish search controls redesign

--- app/helpers/bootstrap/icon_helper.rb
+++ app/helpers/bootstrap/icon_helper.rb
@@ -1,7 +1,7 @@
 module Bootstrap
   module IconHelper
     def icon_tag(name, options = {})
-      classes = Array.wrap(options[:class]) << "ion-#{'ios-' if name !~ /\Asocial/}#{name}"
+      classes = Array.wrap(options[:class]) << "ion-#{'ios-' if name !~ /\A(android|social)/}#{name}"
       content_tag :span, nil, options.merge(class: classes)
     end
   end
