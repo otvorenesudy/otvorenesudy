7bf1ef93f994db6cc111d8eda3292cc74c37341b
Pavol Zbell <pavol.zbell@gmail.com>
2016-08-12 16:36:04 +0200

    Clean up

--- app/helpers/bootstrap/tag_helper.rb
+++
@@ -1,7 +0,0 @@
-module Bootstrap
-  module TagHelper
-    def data_attributes(attributes = {})
-      attributes.inject({}) { |a, (k, v)| a["data-#{k}"] = v; a }
-    end
-  end
-end
--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -9,8 +9,8 @@ module FacetsHelper
   def link_to_collapse_facet_results(facet)
     target = "##{facet.id}-list-more"
 
-    link_to(t('search.facets.show_more'), '#', data_attributes(toggle: :collapse, target: target, collapse: :unfold).merge(class: 'facet-results-more collapsed')) +
-    link_to(t('search.facets.show_less'), '#', data_attributes(toggle: :collapse, target: target, collapse: :fold).merge(class: 'facet-results-less collapsed'))
+    link_to(t('search.facets.show_more'), '#', class: 'facet-results-more collapsed', data: { toggle: :collapse, target: target, collapse: :unfold }) +
+    link_to(t('search.facets.show_less'), '#', class: 'facet-results-less collapsed', data: { toggle: :collapse, target: target, collapse: :fold })
   end
 
   def link_to_terms_facet(facet, result, options = {})
