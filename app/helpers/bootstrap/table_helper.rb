86a11b0d4f13769558e86370f0df97b46032f375
Pavol Zbell <pavol.zbell@gmail.com>
2016-08-31 14:15:45 +0200

    Rename table helper methods

--- app/helpers/bootstrap/table_helper.rb
+++ app/helpers/bootstrap/table_helper.rb
@@ -1,10 +1,10 @@
 module Bootstrap
   module TableHelper
-    def sortable_table_tag(options = {}, &block)
+    def table_tag(options = {}, &block)
       content_tag :table, options.merge(data: { sortable: true }), &block
     end
 
-    def sortable_th_tag(content, options = {})
+    def table_header_tag(content, options = {})
       content_tag :th, content, options.merge(data: { sorter: options.delete(:sorter) })
     end
   end
