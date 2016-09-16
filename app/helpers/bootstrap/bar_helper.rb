f468e2131fdaf82d6effb600bcb27fb7c9404b82
Pavol Zbell <pavol.zbell@gmail.com>
2016-09-16 20:51:34 +0200

    Minor fixes to Bootstrap helpers

--- app/helpers/bootstrap/bar_helper.rb
+++ app/helpers/bootstrap/bar_helper.rb
@@ -3,7 +3,7 @@ module Bootstrap
     def bar_link_to(title, path, options = {})
       classes = Array.wrap(options[:class]) << 'nav-item nav-link'
       classes << 'active' if request.fullpath.start_with? path
-      link_to title, path, class: classes
+      link_to title, path, options.merge(class: classes)
     end
   end
 end
--- app/helpers/bootstrap/table_helper.rb
+++ app/helpers/bootstrap/table_helper.rb
@@ -1,11 +1,11 @@
 module Bootstrap
   module TableHelper
     def table_tag(options = {}, &block)
-      content_tag :table, options.merge(data: { sortable: true }), &block
+      content_tag :table, options.deep_merge(data: { sortable: true }), &block
     end
 
     def table_header_tag(content, options = {})
-      content_tag :th, content, options.merge(data: { sorter: options.delete(:sorter) })
+      content_tag :th, content, options.deep_merge(data: { sorter: options.delete(:sorter) })
     end
   end
 end
