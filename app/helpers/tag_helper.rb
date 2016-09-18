98a4320ae0620a06eed8416e20dc88db7cf763c1
Pavol Zbell <pavol.zbell@gmail.com>
2016-09-18 17:43:45 +0200

    Add help_tag helper

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -1,4 +1,10 @@
 module TagHelper
+  def help_tag(key, options = {})
+    options = options.merge class: 'text-undecorated', trigger: 'hover'
+    t("#{key}.title", default: '').tap { |t| options[:title] = t if t.present? }
+    popover_tag icon_tag('help-outline'), t("#{key}.content"), options
+  end
+
   def time_tag(date_or_time, *args, &block)
     options = args.extract_options!
     content = localize date_or_time, options
