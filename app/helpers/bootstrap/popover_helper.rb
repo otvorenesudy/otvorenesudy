349b3109f27c903bcac191cecf1f9b15b3da3530
Pavol Zbell <pavol.zbell@gmail.com>
2016-09-18 17:25:08 +0200

    Place popovers on top by default like tooltips
    However popovers are still triggered by click by default unlike tooltips

--- app/helpers/bootstrap/popover_helper.rb
+++ app/helpers/bootstrap/popover_helper.rb
@@ -1,6 +1,7 @@
 module Bootstrap
   module PopoverHelper
     def popover_tag(name, content, options = {})
+      options[:placement] ||= 'top'
       data = options.extract!(*%i(delay placement trigger)).select { |_, v| v }
       options.deep_merge! data: data.merge(toggle: 'popover', content: content)
       link_to name, '#', options
