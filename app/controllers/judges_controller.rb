6df9dd32591bfdb0a4d10b407661122cb6eefc56
Pavol Zbell <pavol.zbell@gmail.com>
2016-09-26 19:16:27 +0200

    Refactor and simplify judges controller

--- app/controllers/judges_controller.rb
+++ app/controllers/judges_controller.rb
@@ -14,9 +14,12 @@ class JudgesController < SearchController
 
     @media = @judge.context_search[0..9]
 
-    search_judges_for_indicators
-
     flash.now[:danger] << t('judges.show.incomplete') if @judge.incomplete?
+
+    results = Judge.search(params.merge! indicators: true)
+
+    @facets = results.facets
+    @others = params[:name] ? results.to_a.map(&:first) : []
   end
 
   def curriculum
@@ -33,36 +36,9 @@ class JudgesController < SearchController
     send_file_in @judge.cover_letter_path, name: "Motivačný list - #{@judge.name}", escape: false
   end
 
-  def indicators_suggest
-    redirect_to suggest_judges_path(params)
-  end
-
   protected
 
   include FileHelper
-
-
-  def search_judges_for_indicators
-    results = Judge.search(params.merge!(has_indicators: true))
-
-    @facets = results.facets
-    @judges_for_indicators = Hash.new
-
-    if params[:name]
-      color = generate_random_color
-
-      results.each do |result|
-        color = generate_random_color while @judges_for_indicators[color]
-
-        @judges_for_indicators[color] = result
-      end
-    end
-  end
-
-  # TODO rm
-  def generate_random_color(options = {})
-    3.times.map { rand(0..255) }
-  end
 
   private
 
