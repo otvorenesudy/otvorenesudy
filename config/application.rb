0196a5ca65852d3ac00e5db9a8d36e00bea1bec1
Pavol Zbell <pavol.zbell@gmail.com>
2016-10-25 12:36:08 +0200

    Expire all assets

--- config/application.rb
+++ config/application.rb
@@ -77,6 +77,6 @@ module OpenCourts
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '2.0'
   end
 end
