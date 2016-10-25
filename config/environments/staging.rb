df6d3d7ab147c1424f766aefe75b1524a36bf184
Pavol Zbell <pavol.zbell@gmail.com>
2016-10-25 13:14:23 +0200

    Add fingerprints back to asset URLs

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -18,7 +18,7 @@ OpenCourts::Application.configure do
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
