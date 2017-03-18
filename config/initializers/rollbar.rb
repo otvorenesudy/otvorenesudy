474f47d325fd90a5d64f429b1582b9c1f008c0d4
Samuel Molnar <molnar.samuel@gmail.com>
2017-03-18 17:08:53 +0100

    Do not report warnings

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -33,7 +33,10 @@ Rollbar.configure do |config|
   # config.exception_level_filters.merge!('MyCriticalException' => 'critical')
   #
   # You can also specify a callable, which will be called with the exception instance.
-  # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
+  config.exception_level_filters.merge!(
+    'ActionController::RoutingError' => lambda { |e| 'ignore' },
+    'ActiveRecord::RecordNotFound' => lambda { |e| 'ignore' }
+  )
 
   # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
   # is not installed)
