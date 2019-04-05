225c94a574c31910f18c7cf75c1ab59682aa7f50
Pavol Zbell <pavol.zbell@gmail.com>
2019-04-05 09:42:14 +0200

    Clean up branches

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :domain, 'otvorenesudy.sk'
 server fetch(:domain), user: 'deploy', roles: %w{app db web}
 
 set :stage,    :staging
-set :branch,   'staging'
+set :branch,   'master'
 set :app_path, "#{fetch(:application)}_#{fetch(:stage)}"
 set :rails_env, :staging
 set :deploy_to, "/home/deploy/projects/#{fetch(:app_path)}"
