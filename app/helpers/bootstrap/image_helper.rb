e7a45e93f4d305479d1c1d7583dcbd1cb642e182
Pavol Zbell <pavol.zbell@gmail.com>
2019-04-18 13:29:55 +0200

    Add styles for pagination and alerts

--- app/helpers/bootstrap/image_helper.rb
+++ app/helpers/bootstrap/image_helper.rb
@@ -1,16 +1,22 @@
 module Bootstrap
   module ImageHelper
-    SIZES = { 'sm' => '.8rem', '' => '1rem', 'lg' => '1.5rem' }.freeze
+    SIZES = { base: '1rem', sm: '.8rem', lg: '1.5rem' }.freeze
 
     def icon_tag(name, options = {})
-      size = SIZES.fetch(options[:size].to_s, options.delete(:size))
       use = content_tag(:use, nil, 'xlink:href' => "#{asset_path 'icons.svg'}#icons-#{name}")
-      content_tag :svg, use, options.merge(width: size, height: size)
+      content_tag :svg, use, fix_dimensions(options)
     end
 
     def brand_tag(name, options = {})
       use = content_tag(:use, nil, 'xlink:href' => "#{asset_path 'brands.svg'}#brands-#{name}")
-      content_tag :svg, use, options
+      content_tag :svg, use, fix_dimensions(options)
+    end
+
+    private
+
+    def fix_dimensions(options)
+      size = options.delete(:size) || (:base unless options.key?(:width) || options.key?(:height))
+      size ? options.merge(width: SIZES.fetch(size, size), height: SIZES.fetch(size, size)) : options
     end
   end
 end
