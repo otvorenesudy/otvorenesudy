6a117ac0853e7635dde7f33770c692086055d18b
Pavol Zbell <pavol.zbell@gmail.com>
2019-04-19 19:55:58 +0200

    Add styles for popovers and tooltips

--- app/helpers/bootstrap/popover_helper.rb
+++ app/helpers/bootstrap/popover_helper.rb
@@ -3,7 +3,7 @@ module Bootstrap
     def popover_tag(name, content, options = {})
       options[:placement] ||= 'top'
       data = options.extract!(*%i(delay placement trigger)).select { |_, v| v }
-      options.deep_merge! data: data.merge(toggle: 'popover', content: content)
+      options.deep_merge! data: data.merge(content: content, toggle: 'popover'), tabindex: -1
       link_to name, '#', options
     end
   end
