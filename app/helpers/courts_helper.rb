56f2046e4fbd0b029b809bddefebb67995a3adb3
Pavol Zbell <pavol.zbell@gmail.com>
2019-04-22 12:32:31 +0200

    Remove Google Maps support

--- app/helpers/courts_helper.rb
+++ app/helpers/courts_helper.rb
@@ -1,33 +1,4 @@
 module CourtsHelper
-  def courts_map(courts, options = {})
-    key = 'AIzaSyAOxHiahbFCA001J7SFq1C-U6qYUfaKvT8'
-
-    courts = Array.wrap courts
-    id = "map-#{courts.hash.abs}"
-
-    groups = courts.group_by(&:coordinates)
-    markers = Gmaps4rails.build_markers groups.keys do |coordinates, marker|
-      marker.lat coordinates[:latitude]
-      marker.lng coordinates[:longitude]
-      marker.infowindow render(partial: 'courts/map/marker', locals: coordinates.merge(courts: groups[coordinates].sort_by(&:name))).html_safe
-    end
-
-    classes = Array.wrap(options[:class]).unshift 'map'
-    options = options.except(:class).reverse_merge(disableDefaultUI: false, zoom: 16)
-
-    content_for :scripts do
-      content_tag :script, nil, type: 'text/javascript', src: "https://maps.google.com/maps/api/js?v=3.24&key=#{key}"
-    end
-
-    content_for :scripts do
-      content_tag :script, type: 'text/javascript' do
-        render(partial: 'courts/map/handler', formats: :js, locals: { id: id, options: options, markers: markers }).html_safe
-      end
-    end
-
-    content_tag :div, nil, id: id, class: classes
-  end
-
   def link_to_court(court, options = {})
     link_to court.name, court_path(court), options
   end
