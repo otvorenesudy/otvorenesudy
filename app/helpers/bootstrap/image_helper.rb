423af276b9d6bc5bf155c580513f5dd5ff1a42d0
Pavol Zbell <pavol.zbell@gmail.com>
2019-04-25 00:32:19 +0200

    Add support for image variants

--- app/helpers/bootstrap/image_helper.rb
+++ app/helpers/bootstrap/image_helper.rb
@@ -2,21 +2,21 @@ module Bootstrap
   module ImageHelper
     SIZES = { base: 16, sm: 12, lg: 20 }.with_indifferent_access.freeze
 
-    def icon_tag(name, options = {})
-      use = content_tag(:use, nil, 'xlink:href' => "#{asset_path 'icons.svg'}#icons-#{name}")
-      content_tag :svg, use, fix_dimensions(options)
+    def icon_tag(name, options = {}, variants = {})
+      use_within_svg_tag 'icons', name, options, variants
     end
 
-    def brand_tag(name, options = {})
-      use = content_tag(:use, nil, 'xlink:href' => "#{asset_path 'brands.svg'}#brands-#{name}")
-      content_tag :svg, use, fix_dimensions(options)
+    def brand_tag(name, options = {}, variants = {})
+      use_within_svg_tag 'brands', name, options, variants
     end
 
     private
 
-    def fix_dimensions(options)
+    def use_within_svg_tag(source, identifier, options = {}, variants = {})
+      options = options.merge(variants.fetch(variant = options.delete(:variant), {}))
       size = options.delete(:size) || (:base unless options.key?(:width) || options.key?(:height))
-      size ? options.merge(width: SIZES.fetch(size, size), height: SIZES.fetch(size, size)) : options
+      use = content_tag :use, nil, { 'xlink:href' => "#{asset_path "#{source}.svg"}##{source}-#{identifier}#{".#{variant}" if variant}" }
+      content_tag :svg, use, size ? options.merge(width: SIZES.fetch(size, size), height: SIZES.fetch(size, size)) : options
     end
   end
 end
