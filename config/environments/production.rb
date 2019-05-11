69fdc6b538db54ce155f7f8216cbc8f960be7088
Samuel Molnar <molnar.samuel@gmail.com>
2019-05-12 00:15:20 +0200

    Precompile assets on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,13 +15,10 @@ OpenCourts::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
-  # Precompile SVG
-  config.assets.precompile += %w[*.svg]
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
