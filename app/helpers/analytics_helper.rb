4da5272554a1f86d2485b531773e76f23f07aaab
Pavol Zbell <pavol.zbell@gmail.com>
2019-05-14 21:02:00 +0200

    Support more signatures in event tracking helper

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,7 +1,13 @@
 # See https://developers.google.com/analytics/devguides/collection/analyticsjs/events
 
 module AnalyticsHelper
-  def track(category, action, label = nil)
+  def track(category, action = 'click', label = nil)
+    if category.is_a?(Hash)
+      category, action, label = category.fetch(:in, File.basename(@virtual_path)).titleize.strip, *category.except(:in).first
+      action, label = 'navigate', "item:#{label}" if action == :nav
+      action = 'click' if action == :as
+    end
+
     { 'on' => 'click,auxclick,contextmenu', 'event-category' => category, 'event-action' => action, 'event-label' => label }.select { |_, v| v.present? }
   end
 end
