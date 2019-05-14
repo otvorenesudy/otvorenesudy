3524f86c83068d5780c26bffac5a2351d2af0c18
Pavol Zbell <pavol.zbell@gmail.com>
2019-05-14 13:31:45 +0200

    Update event tracking helper

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,9 +1,7 @@
-# See https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide
+# See https://developers.google.com/analytics/devguides/collection/analyticsjs/events
 
 module AnalyticsHelper
-  def track(category, action, label = nil, options = {})
-    category, action, label = *[category, action, label].map { |s| Array.wrap(s).map(&:to_s).join(' - ').downcase }
-    data = { :'track-category' => category, :'track-action' => action, :'track-label' => label }
-    options.deep_merge data: data.select { |_, v| v.present? }
+  def track(category, action, label = nil)
+    { 'on' => 'click,auxclick,contextmenu', 'event-category' => category, 'event-action' => action, 'event-label' => label }.select { |_, v| v.present? }
   end
 end
