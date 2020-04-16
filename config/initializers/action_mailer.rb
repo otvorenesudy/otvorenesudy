1c7e64aae8b616313cf9b06d9c32e0baff73b4bd
Samuel Molnar <molnar.samuel@gmail.com>
2020-04-16 23:09:03 +0200

    Remove data notice and update smtp mailer

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,11 +1,12 @@
 unless Rails.env.test?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.mailgun.org',
-    domain: 'mg.otvorenesudy.sk',
+    address: 'smtp.sendgrid.net',
+    domain: 'otvorenesudy.sk',
     port: 587,
     user_name: Configuration.mailer.username,
     password: Configuration.mailer.password,
-    authentication: :plain
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
