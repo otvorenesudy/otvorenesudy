59e2676ad48e06e888fbf555c317c0d622eb42a3
Samuel Molnar <molnar.samuel@gmail.com>
2020-04-21 11:23:28 +0200

    Fix mailer email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = ::Configuration.mailer.username
+  config.mailer_sender = 'noreply@otvorenesudy.sk'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
