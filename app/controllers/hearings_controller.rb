7efaff36d4bd89019ac05dd1a9c3a723e85dc001
Samuel Molnar <molnar.samuel@gmail.com>
2024-03-31 18:22:32 +0200

    Turn off bot protection

--- app/controllers/hearings_controller.rb
+++ app/controllers/hearings_controller.rb
@@ -1,21 +1,19 @@
 class HearingsController < SearchController
-  before_filter :verify
-
   def show
     @hearing = Hearing.find(params[:id])
 
-    @type   = @hearing.type
-    @court  = @hearing.court
+    @type = @hearing.type
+    @court = @hearing.court
     @judges = @hearing.judges.order(:last, :middle, :first)
 
-    @proposers  = @hearing.proposers.order(:name)
-    @opponents  = @hearing.opponents.order(:name)
+    @proposers = @hearing.proposers.order(:name)
+    @opponents = @hearing.opponents.order(:name)
     @defendants = @hearing.defendants.order(:name)
 
-    flash.now[:danger]  << t('hearings.show.unprocessed') if @hearing.unprocessed?
-    flash.now[:danger]  << t('hearings.show.future_date') if @hearing.has_future_date?
+    flash.now[:danger] << t('hearings.show.unprocessed') if @hearing.unprocessed?
+    flash.now[:danger] << t('hearings.show.future_date') if @hearing.has_future_date?
     flash.now[:warning] << t('hearings.show.faulty_date') if @hearing.had_future_date?
-    flash.now[:warning] << t('hearings.show.anonymized')  if @hearing.anonymized?
+    flash.now[:warning] << t('hearings.show.anonymized') if @hearing.anonymized?
   end
 
   # TODO rm - unused?
@@ -46,6 +44,6 @@ class HearingsController < SearchController
   def search_associations
     # NOTE do not eager load scoped associations after original associations,
     # e.g. :exact_judges has to go before :judges, otherwise scoped association will not be loaded
-    [:form, :subject, :exact_judges, :inexact_judgings, :judges, :opponents, :proposers, :defendants, :court]
+    %i[form subject exact_judges inexact_judgings judges opponents proposers defendants court]
   end
 end
