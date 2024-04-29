e2255c685fb550b51f800b6ebb172ba5857faa18
Samuel Molnar <molnar.samuel@gmail.com>
2024-04-29 10:52:34 +0200

    Rename master to main

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,9 @@
 set :domain, 'otvorenesudy.sk'
 
-server fetch(:domain), user: 'deploy', roles: %w{app db web}
+server fetch(:domain), user: 'deploy', roles: %w[app db web]
 
-set :stage,    :staging
-set :branch,   'master'
+set :stage, :staging
+set :branch, 'main'
 set :app_path, "#{fetch(:application)}-#{fetch(:stage)}"
 set :rails_env, :staging
 set :deploy_to, "/home/deploy/projects/#{fetch(:app_path)}"
