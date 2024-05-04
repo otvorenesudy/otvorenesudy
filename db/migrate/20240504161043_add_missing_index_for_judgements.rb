f1642abf14fae8fb4b80d56a89fbacd515803627
Samuel Molnar <molnar.samuel@gmail.com>
2024-05-04 18:21:11 +0200

    Simplify migration

--- db/migrate/20240504161043_add_missing_index_for_judgements.rb
+++ db/migrate/20240504161043_add_missing_index_for_judgements.rb
@@ -1,14 +1,5 @@
 class AddMissingIndexForJudgements < ActiveRecord::Migration
   def up
-    judgements = Judgement.group(:decree_id, :judge_name_unprocessed).having('count(*) > 1').count
-
-    judgements.each do |(decree_id, judge_name_unprocessed), _|
-      id, *duplicate_ids =
-        Judgement.where(decree_id: decree_id, judge_name_unprocessed: judge_name_unprocessed).order(:id).pluck(:id)
-
-      Judgement.where(id: duplicate_ids).delete_all
-    end
-
     add_index :judgements, %i[decree_id judge_name_unprocessed], unique: true
   end
 
