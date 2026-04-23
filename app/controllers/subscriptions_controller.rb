e613ce8e58ed589518382fba994bfbac1883380d
Samuel Molnar <molnar.samuel@gmail.com>
2026-04-23 17:58:26 +0200

    Fix notice

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -47,7 +47,7 @@ class SubscriptionsController < ApplicationController
 
     @subscription.destroy if @subscription
 
-    flash[:notice] = t('.subscriptions.unsubscribe.notice')
+    flash[:notice] = t('.subscriptions.mailer.results.unsubscribe.notice')
 
     redirect_to root_path
   end
